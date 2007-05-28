5cea703db635484d6cc0c2c5086bf8746cfeb374
Jon Rafkind <jon@rafkind.com>
2007-05-28 22:06:02 +0000

    add scrollable window that can be drawn on

--- editor/src/com/rafkind/paintown/DirectoryModel.java
+++ editor/src/com/rafkind/paintown/DirectoryModel.java
@@ -1,24 +1,44 @@
 package com.rafkind.paintown;
 
 import java.io.File;
+import java.util.Iterator;
 import java.util.List;
 import java.util.ArrayList;
 
 import javax.swing.ListModel;
 import javax.swing.event.ListDataListener;
+import javax.swing.event.ListDataEvent;
 
 public class DirectoryModel implements ListModel {
 
-	private File[] files;
+	private List files;
 	private List listeners;
 	
 	public DirectoryModel( String dir ){
-		setDirectory( dir );
 		listeners = new ArrayList();
+		files = new ArrayList();
+		setDirectory( dir );
+	}
+
+	public void setDirectory( File dir ){
+		if ( dir.isDirectory() ){
+			files.clear();
+			files.add( new File( dir.getPath() + "/.." ) );
+			File[] fs = dir.listFiles();
+			for ( int i = 0; i < fs.length; i++ ){
+				files.add( fs[ i ] );
+			}
+
+			ListDataEvent event = new ListDataEvent( this, ListDataEvent.CONTENTS_CHANGED, 0, 999999 );
+			for ( Iterator it = listeners.iterator(); it.hasNext(); ){
+					ListDataListener l = (ListDataListener) it.next();
+					l.contentsChanged( event );
+			}
+		}
 	}
 
 	public void setDirectory( String dir ){
-		this.files = new File( dir ).listFiles();
+		setDirectory( new File( dir ) );
 	}
 
 	public void addListDataListener( ListDataListener listener ){
@@ -26,11 +46,11 @@ public class DirectoryModel implements ListModel {
 	}
 
 	public Object getElementAt( int index ){
-		return this.files[ index ];
+		return this.files.get( index );
 	}
 
 	public int getSize(){
-		return this.files.length;
+		return this.files.size();
 	}
 
 	public void removeListDataListener( ListDataListener listener ){
--- editor/src/com/rafkind/paintown/Editor.java
+++ editor/src/com/rafkind/paintown/Editor.java
@@ -1,15 +1,21 @@
 package com.rafkind.paintown;
 
 import java.util.*;
+import java.awt.*;
+import java.awt.image.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.event.*;
+import java.io.*;
 
 import org.swixml.SwingEngine;
 
 public class Editor extends JFrame {
 
+	private Image levelImage;
+
 	public Editor(){
-		this.setSize( 1000, 600 );
+		this.setSize( 900, 600 );
 
 		JMenuBar menuBar = new JMenuBar();
 		JMenu menuProgram = new JMenu( "Program" );
@@ -23,6 +29,11 @@ public class Editor extends JFrame {
 			}
 		};
 
+		levelImage = new BufferedImage( 1000, 300, BufferedImage.TYPE_INT_RGB );
+		Graphics g = levelImage.getGraphics();
+		g.setColor( new Color( 64, 192, 54 ) );
+		g.fillRect( 10, 10, 200, 200 );
+
 		quit.addActionListener( new ActionListener(){
 			public void actionPerformed( ActionEvent event ){
 				closeHook.invoke_();
@@ -31,12 +42,173 @@ public class Editor extends JFrame {
 
 		SwingEngine engine = new SwingEngine( "main.xml" );
 		this.getContentPane().add( (JPanel) engine.getRootComponent() );
+		
+		final JPanel viewContainer = (JPanel) engine.find( "view" );
+		final JScrollPane viewScroll = new JScrollPane( JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS );
+		final JPanel view = new MyCanvas(){
+			protected void paintComponent( Graphics g ){
+				renderLevel( g,
+				             viewScroll.getHorizontalScrollBar().getValue(),
+				             viewScroll.getVerticalScrollBar().getValue() );
+				// g.drawImage( levelImage, 0, 0, Color.WHITE, null );
+			}
+		};
+		viewScroll.setPreferredSize( new Dimension( 200, 200 ) );
+		viewScroll.setViewportView( view );
+		viewScroll.getHorizontalScrollBar().setBackground( new Color( 128, 255, 0 ) );
+		// viewScroll.getHorizontalScrollBar().setForeground( new Color( 255, 128, 0 ) );
+		System.out.println( "Maximum size: " + viewScroll.getHorizontalScrollBar().getMaximum() );
+		System.out.println( "Viewable size: " + viewScroll.getHorizontalScrollBar().getVisibleAmount() );
+		viewScroll.getHorizontalScrollBar().addAdjustmentListener( new AdjustmentListener(){
+			public void adjustmentValueChanged( AdjustmentEvent e ){
+				System.out.println( "New horizontal: " + viewScroll.getHorizontalScrollBar().getValue() );
+			}
+		});
+		System.out.println( "Horizontal value: " + viewScroll.getHorizontalScrollBar().getValue() );
+		GridBagLayout layout = new GridBagLayout();
+		viewContainer.setLayout( layout );
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.fill = GridBagConstraints.BOTH;
+		
+		/*
+		constraints.anchor = GridBagConstraints.NORTHWEST;
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		*/
+		constraints.weightx = 1;
+		constraints.weighty = 1;
+		layout.setConstraints( viewScroll, constraints );
+		view.setBorder( BorderFactory.createLineBorder( new Color( 255, 0, 0 ) ) );
+		viewContainer.add( viewScroll );
 
-		JList list = (JList) engine.find( "files" );
-		list.setModel( new DirectoryModel( "data" ) );
+		final JList list = (JList) engine.find( "files" );
+		final DirectoryModel model = new DirectoryModel( "data" );
+		list.setModel( model );
+
+		list.addMouseListener( new MouseAdapter(){
+			public void mouseClicked( MouseEvent event ){
+				if ( event.getClickCount() == 2 ){
+					int i = list.locationToIndex( event.getPoint() );
+					if ( i != -1 ){
+						File f = (File) model.getElementAt( i );
+						if ( f.isDirectory() ){
+							model.setDirectory( f );
+						} else {
+							loadLevel( f );
+						}
+					}
+				}
+			}
+		});
 
 		this.setJMenuBar( menuBar );
 		this.addWindowListener( new CloseHook( closeHook ) );
+	}
+
+	private class MyCanvas extends JPanel implements Scrollable {
+		public MyCanvas(){
+			super();
+		}
+
+		public Dimension getPreferredSize(){
+			return new Dimension( 1000, 480 );
+		}
+
+		public Dimension getPreferredScrollableViewportSize(){
+			return getPreferredSize();
+		}
+
+		public int getScrollableBlockIncrement(Rectangle visibleRect, int orientation, int direction){
+		 	return 10;
+		}
+
+		public boolean getScrollableTracksViewportHeight(){
+			return false;
+		}
+
+		public boolean getScrollableTracksViewportWidth(){
+			return false;
+		}
+
+		public int getScrollableUnitIncrement(Rectangle visibleRect, int orientation, int direction){
+			return 1;
+		}
+	}
+
+	private void loadLevel( File f ){
+	}
+
+	private void renderLevel( Graphics g, int x, int y ){
+		Image i = new BufferedImage( 640, 480, BufferedImage.TYPE_INT_RGB );
+		Graphics ig = i.getGraphics();
+		ig.setColor( new Color( 255, 0, 0 ) );
+		ig.fillOval( 50, 50, 50, 50 );
+		g.drawImage( i, x, y, Color.WHITE, null );
+	}
+
+	private class ShitRange implements BoundedRangeModel {
+		private int extent;
+		private int maximum;
+		private int minimum;
+		private int value;
+		private boolean adjusting;
+
+		public ShitRange(){
+		}
+
+		public void addChangeListener(ChangeListener x){
+		}
+
+		public int getExtent(){
+			return extent;
+		}
+
+		public int getMaximum(){
+			return maximum;
+		}
+
+		public int getMinimum(){
+			return minimum;
+		}
+
+		public int getValue(){
+			return value;
+		}
+
+		public boolean getValueIsAdjusting(){
+			return adjusting;
+		}
+
+		public void removeChangeListener(ChangeListener x){
+		}
+
+		public void setExtent(int newExtent){
+			extent = newExtent;
+		}
+
+		public void setMaximum(int newMaximum){
+			maximum = newMaximum;
+		}
+
+		public void setMinimum(int newMinimum){
+			minimum = newMinimum;
+		}
+
+		public void setRangeProperties(int value, int extent, int min, int max, boolean adjusting){
+			this.value = value;
+			this.extent = extent;
+			this.minimum = min;
+			this.maximum = max;
+			this.adjusting = adjusting;
+		}
+
+		public void setValue(int newValue){
+			value = newValue;
+		}
+
+		public void setValueIsAdjusting(boolean b){
+			adjusting = b;
+		}
 	}
 
 	public static void main( String[] args ){
