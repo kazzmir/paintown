a361a7b51b2a489cfb2521f1ea82c07537dc757d
Jon Rafkind <jon@rafkind.com>
2007-06-17 19:10:46 +0000

    use swixml to load character dialog box

--- editor/src/com/rafkind/paintown/CharacterEditor.java
+++ editor/src/com/rafkind/paintown/CharacterEditor.java
@@ -5,11 +5,14 @@ import javax.swing.*;
 import com.rafkind.paintown.level.Character;
 import com.rafkind.paintown.level.Level;
 
+import org.swixml.SwingEngine;
+
 public class CharacterEditor implements PropertyEditor {
 	public CharacterEditor( Character i ){
 	}
 
 	public JComponent createPane( Level level ){
-		return new JPanel();
+		final SwingEngine engine = new SwingEngine( "character.xml" );
+		return (JPanel) engine.getRootComponent();
 	}
 }
