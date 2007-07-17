16942b954096323ffd6147681981ef882dfb845b
Jon Rafkind <jon@rafkind.com>
2007-07-17 18:34:38 +0000

    add robert and yamazaki to default characters

--- editor/src/com/rafkind/paintown/Editor.java
+++ editor/src/com/rafkind/paintown/Editor.java
@@ -202,6 +202,8 @@ public class Editor extends JFrame {
 		data.add( new File( "data/chars/shermie/shermie.txt" ) );
 		data.add( new File( "data/chars/yashiro/yashiro.txt" ) );
 		data.add( new File( "data/chars/jhun/jhun.txt" ) );
+		data.add( new File( "data/chars/robert/robert.txt" ) );
+		data.add( new File( "data/chars/yamazaki/yamazaki.txt" ) );
 		data.add( new File( "data/misc/apple/apple.txt" ) );
 		return data;
 	}
