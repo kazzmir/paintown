bce159832828ceb9a99f8903e8ca84b414a84395
Jon Rafkind <jon@rafkind.com>
2007-08-15 05:41:24 +0000

    use jcomponent instead of awts canvas

--- editor/src/com/rafkind/paintown/animator/DrawArea.java
+++ editor/src/com/rafkind/paintown/animator/DrawArea.java
@@ -15,7 +15,7 @@ import org.swixml.SwingEngine;
 import com.rafkind.paintown.animator.*;
 import com.rafkind.paintown.*;
 
-public final class DrawArea extends Canvas
+public final class DrawArea extends JComponent
 {
 	private BufferedImage img;
 	private int img_x;
@@ -28,20 +28,22 @@ public final class DrawArea extends Canvas
 	private int delay;
 	private boolean _delayChanged;
 	private BoundingBox attackArea = new BoundingBox(0,0,0,0);
-	
+
 	public Dimension getPreferredSize()
 	{
 		return new Dimension(640,480);
 	}
 
-	public void paint( Graphics g ){
-      
-			g.setColor( new Color( 0, 0, 0 ) );
-			g.fillRect( 0, 0, 640, 480 );
-			g.setColor( new Color( 255, 255, 0 ) );
-			g.drawLine(0,350,640,350);
-			if(img != null)g.drawImage( img, x + img_x + offset_x, y + img_y + offset_y, null );
-			if(!attackArea.empty())attackArea.render(g, x + img_x + offset_x, y + img_y + offset_y);
+	public void paintComponent( Graphics g ){
+
+		g.setColor( new Color( 0, 0, 0 ) );
+		g.fillRect( 0, 0, 640, 480 );
+		g.setColor( new Color( 255, 255, 0 ) );
+		g.drawLine(0,350,640,350);
+		if(img != null)
+			g.drawImage( img, x + img_x + offset_x, y + img_y + offset_y, null );
+		if(!attackArea.empty())
+			attackArea.render(g, x + img_x + offset_x, y + img_y + offset_y);
 	}
 	
 	public void setImage(BufferedImage i)
