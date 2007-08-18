cacfdabd0a8a8e057da6e23f2eb449a32ff893cc
juvinious <juvinious@gmail.com>
2007-08-18 20:45:28 +0000

    Added fiximage.sh which uses convert to fix images.

---
+++ editor/misc/fiximage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for f in `ls ./`;do
+	if [ -d "$f" ]; then
+		# bleh not directories
+		echo "skipping directory"
+	else
+		`convert -fill "rgb(255,0,255)" -opaque transparent -draw "color 0,0 floodfill" -trim $f new-$f`
+		echo "Converted $f to new-$f"
+	fi
+done
+
