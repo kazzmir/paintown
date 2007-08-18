f320efe8f07906863b8e756b01fbb90baf2d225f
Jon Rafkind <jon@rafkind.com>
2007-08-18 21:58:02 +0000

    use || in if

--- editor/misc/fiximage.sh
+++ editor/misc/fiximage.sh
@@ -3,10 +3,7 @@
 # I guess imagemagick can support other formats... if you need one add it to the list
 
 for f in $(find ./ -iname "*.png" -or -iname "*.bmp" -or -iname "*.jpg" -or -iname "*.jpeg" -or -iname "*.gif" | sed -e 's/\.\///g');do
-	if [ "$1" == "replace" ]; then
-		convert -fill "rgb(255,0,255)" -opaque transparent -draw "color 0,0 floodfill" -trim $f $f
-		echo "Converted $f"
-	elif [ "$1" == "-r" ]; then
+	if [ "x$1" = "xreplace" ] || [ "x$1" = "-r" ]; then
 		convert -fill "rgb(255,0,255)" -opaque transparent -draw "color 0,0 floodfill" -trim $f $f
 		echo "Converted $f"
 	else
