96bae378c0749d54fdffb185aeb04b9dd5675a48
juvinious <juvinious@gmail.com>
2007-08-18 21:52:06 +0000

    Made fiximage use specific file formats and gave it some options

--- editor/misc/fiximage.sh
+++ editor/misc/fiximage.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-for f in `ls ./`;do
-	if [ -d "$f" ]; then
-		# bleh not directories
-		echo "skipping directory"
+# I guess imagemagick can support other formats... if you need one add it to the list
+
+for f in $(find ./ -iname "*.png" -or -iname "*.bmp" -or -iname "*.jpg" -or -iname "*.jpeg" -or -iname "*.gif" | sed -e 's/\.\///g');do
+	if [ "$1" == "replace" ]; then
+		convert -fill "rgb(255,0,255)" -opaque transparent -draw "color 0,0 floodfill" -trim $f $f
+		echo "Converted $f"
+	elif [ "$1" == "-r" ]; then
+		convert -fill "rgb(255,0,255)" -opaque transparent -draw "color 0,0 floodfill" -trim $f $f
+		echo "Converted $f"
 	else
 		convert -fill "rgb(255,0,255)" -opaque transparent -draw "color 0,0 floodfill" -trim $f new-$f
 		echo "Converted $f to new-$f"
