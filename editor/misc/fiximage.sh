d11464a9b25a48daf6dc7b416f2a9ac03e366a56
juvinious <juvinious@gmail.com>
2007-08-18 22:03:37 +0000

    Missing x-r in fiximage

--- editor/misc/fiximage.sh
+++ editor/misc/fiximage.sh
@@ -3,7 +3,7 @@
 # I guess imagemagick can support other formats... if you need one add it to the list
 
 for f in $(find ./ -iname "*.png" -or -iname "*.bmp" -or -iname "*.jpg" -or -iname "*.jpeg" -or -iname "*.gif" | sed -e 's/\.\///g');do
-	if [ "x$1" = "xreplace" ] || [ "x$1" = "-r" ]; then
+	if [ "x$1" = "xreplace" ] || [ "x$1" = "x-r" ]; then
 		convert -fill "rgb(255,0,255)" -opaque transparent -draw "color 0,0 floodfill" -trim $f $f
 		echo "Converted $f"
 	else
