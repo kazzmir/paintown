80fc87a3d45dc9cc857d14111b8a57a872bb2993
Jon Rafkind <jon@rafkind.com>
2007-11-02 03:00:11 +0000

    maybe improve fog a little

--- src/environment/fog_atmosphere.cpp
+++ src/environment/fog_atmosphere.cpp
@@ -17,10 +17,17 @@ Atmosphere(){
 	fog->fill( Bitmap::MaskColor );
 	fog->circleFill( 25, 25, 20, Bitmap::makeColor( 0xbb, 0xbb, 0xcc ) );
 
-	for ( int i = 0; i < screenX(); i += 20 ){
+	for ( int i = -20; i < screenX(); i += 20 ){
+		fogs.push_back( new Fog( i + Util::rnd( 9 ) - 4, screenY() - 30, Util::rnd( 360 ) ) );
+		fogs.push_back( new Fog( i + Util::rnd( 9 ) - 4, screenY() - 40, Util::rnd( 360 ) ) );
+		fogs.push_back( new Fog( i + Util::rnd( 9 ) - 4, screenY() - 40, Util::rnd( 360 ) ) );
+		fogs.push_back( new Fog( i + Util::rnd( 9 ) - 4, screenY() - 50, Util::rnd( 360 ) ) );
+		fogs.push_back( new Fog( i + Util::rnd( 9 ) - 4, screenY() - 60, Util::rnd( 360 ) ) );
+		/*
 		for ( int q = 0; q < 3; q++ ){
 			fogs.push_back( new Fog( i + Util::rnd( 9 ) - 4, screenY() - Util::rnd( 30 ) - 40, Util::rnd( 360 ) ) );
 		}
+		*/
 	}
 }
 
@@ -35,7 +42,7 @@ void FogAtmosphere::draw( Bitmap * work ){
 	Bitmap::transBlender( 0, 0, 0, 64 );
 	for ( vector< Fog * >::iterator it = fogs.begin(); it != fogs.end(); it++ ){
 		Fog * f = *it;
-		int y = (int)(f->y + sin( f->ang * 3.14159 / 180.0 ) * 4);
+		int y = (int)(f->y + sin( f->ang * 3.14159 / 180.0 ) * 2);
 		fog->drawTrans( f->x, y, *work );
 	}
 	/*
