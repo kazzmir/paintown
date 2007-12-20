64272afaf363610d2790bc32410e477880e1484d
Jon Rafkind <jon@rafkind.com>
2007-12-20 06:11:51 +0000

    wrap lines in message box

--- src/network/messages.cpp
+++ src/network/messages.cpp
@@ -1,6 +1,7 @@
 #include "util/bitmap.h"
 #include "messages.h"
 #include "util/font.h"
+#include "globals.h"
 
 using namespace std;
 	
@@ -12,6 +13,43 @@ height( height ){
 void Messages::addMessage( const std::string & s ){
 	messages.push_back( s );
 }
+
+static vector< string > wrapStrings( const string & left, const string & right, const Font & font, int max, vector< string > accum ){
+	if ( left == "" ){
+		return accum;
+	}
+
+	int length = font.textLength( left.c_str() );
+
+	if ( length >= max ){
+		return wrapStrings( left.substr( 0, left.length() / 2 ), left.substr( left.length() / 2 ) + right, font, max, accum );
+	} else if ( length >= max - font.textLength( "E" ) || right == "" ){
+		accum.push_back( left );
+		return wrapStrings( right, "", font, max, accum );
+	} else {
+		return wrapStrings( left + right.substr( 0, 1 ), right.substr( 1 ), font, max, accum );
+	}
+}
+
+/*
+static int wrapPrint( const Bitmap & area, int y, int top, const Font & font, const string & left, const string & right ){
+	if ( left == "" ){
+		return top;
+	}
+
+	int length = font.textLength( left.c_str() );
+	int max = area.getWidth();
+
+	if ( length >= max ){
+		return wrapPrint( area, y - font.getHeight(), top - font.getHeight(), font, left.substr( 0, left.length() / 2 ), left.substr( left.length() / 2 ) + right );
+	} else if ( length >= max - font.textLength( "E" ) || right == "" ){
+		font.printf( 0, y, Bitmap::makeColor( 255, 255, 255 ), area, left, 0 );
+		return wrapPrint( area, y + font.getHeight(), top, font, right, "" );
+	} else {
+		return wrapPrint( area, y, top, font, left + right.substr( 0, 1 ), right.substr( 1 ) );
+	}
+}
+*/
 	
 void Messages::draw( int x, int y, const Bitmap & work, const Font & font ){
 	work.drawingMode( Bitmap::MODE_TRANS );
@@ -22,9 +60,33 @@ void Messages::draw( int x, int y, const Bitmap & work, const Font & font ){
 
 	Bitmap area( work, x, y, width, height );
 	int current_y = height - font.getHeight() - 1;
+	// int max_length = area.getWidth() / font.textLength( "E" );
 	for ( vector< string >::reverse_iterator it = messages.rbegin(); it != messages.rend(); it++ ){
+		if ( current_y < 0 - font.getHeight() ){
+			break;
+		}
 		const string & s = *it;
-		font.printf( 0, current_y, Bitmap::makeColor( 255, 255, 255 ), area, s, 0 );
+		vector< string > all;
+		all = wrapStrings( s, "", font, area.getWidth(), all );
+		current_y -= font.getHeight() * (all.size() - 1);
+		int my = current_y;
+		for ( vector< string >::iterator str = all.begin(); str != all.end(); str++ ){
+			font.printf( 0, my, Bitmap::makeColor( 255, 255, 255 ), area, *str, 0 );
+			my += font.getHeight();
+
+		}
+		// current_y = wrapPrint( area, current_y, current_y, font, s, "" );
+		/*
+		int lines = font.textLength( s.c_str() ) / area.getWidth();
+		current_y -= font.getHeight() * lines;
+		int my = current_y;
+		while ( s != "" ){
+			string sub = s.substr( 0, max_length );
+			s.erase( 0, max_length );
+			font.printf( 0, my, Bitmap::makeColor( 255, 255, 255 ), area, sub, 0 );
+			my += font.getHeight();
+		}
+		*/
 		current_y -= font.getHeight();
 	}
 }
