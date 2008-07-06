92872f48520837f7143c99961e85223bf420cb69
Jon Rafkind <jon@rafkind.com>
2008-07-06 21:30:04 +0000

    add some comments

--- src/network/chat_server.h
+++ src/network/chat_server.h
@@ -86,23 +86,34 @@ class ChatServer: public sigslot::has_slots<> {
 public:
 	ChatServer( const std::string & name, Network::Socket socket );
 
+	/* run the chat server - accept connections and handle input */
 	virtual void run();
 
+	/* add a connection from a client */
 	virtual void addConnection( Network::Socket socket );
 
 	Network::Socket getSocket(){
 		return socket;
 	}
 
+	/* end a connection */
 	void killClient( Client * c );
 
+	/* add a message to the chat box and send it out */
 	void addMessage( const std::string & s, unsigned int id );
+	
+	/* send a message from the guy who is identified by id */
 	void sendMessage( const Network::Message & message, unsigned int id );
 	
+	/* need to update the screen because a message came through or
+	 * the user typed something in the chat box
+	 */
 	void needUpdate();
 		
+	/* is accepting connections from clients */
 	bool isAccepting();
 
+	/* get the list of connected clients */
 	vector< Network::Socket > getConnectedClients();
 
 	inline const std::string getName() const {
@@ -112,21 +123,43 @@ public:
 	virtual ~ChatServer();
 
 protected:
+	/* force kill all client threads */
 	void killAllClients();
+
+	/* notify clients that the game is about to start */
 	void shutdownClientThreads();
+
+	/* do chat logic */
 	bool logic( Keyboard & keyboard );
+
+	/* true if a redraw is needed */
 	bool needToDraw();
+
+	/* draw things to work */
 	void draw( const Bitmap & work );
 	
+	/* draw the input box */
 	void drawInputBox( int x, int y, const Bitmap & work );
+
+	/* draw the buddy list */
 	void drawBuddyList( int x, int y, const Bitmap & work, const Font & font );
+
+	/* get the next widget to be focused */
 	Focus nextFocus( Focus f );
+
+	/* accept input from the keyboard */
 	void handleInput( Keyboard & keyboard );
+
+	/* get the color a focused widget should be drawn with */
 	int focusColor( Focus f );
+
+	/* stop accepting new connections */
 	void stopAccepting();
 
+	/* used to start threads in the run() method instead of elsewhere */
 	void startThreadsHack();
 
+	/* return a new connection id */
 	inline const int clientId(){
 		return client_id++;
 	}
