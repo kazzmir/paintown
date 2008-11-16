994fabb839b92fc527f60f03becb1bd27125f608
Jon Rafkind <jon@rafkind.com>
2008-11-16 21:49:35 +0000

    load module and import it

--- src/modules/python.cpp
+++ src/modules/python.cpp
@@ -4,6 +4,8 @@
 #include "python.h"
 #include <string>
 #include "../globals.h"
+#include "../util/funcs.h"
+#include <sstream>
 
 #include <Python.h>
 
@@ -12,11 +14,22 @@ using namespace std;
 PythonEngine::PythonEngine(const string & path):
 Script::Engine(),
 path(path){
-    Global::debug(0) << "Loading python.." << endl;
+    Global::debug(1) << "Loading python.." << endl;
     Py_Initialize();
-    Global::debug(0) << "Load module " << path << endl;
-    PyObject* module = PyImport_ImportModule(path.c_str());
-    Global::debug(0) << "Loaded " << module << endl;
+    Global::debug(1) << "Load module " << path << endl;
+    ostringstream python_string;
+    python_string << "x = \"" << (Util::getDataPath() + path) << "\"; import sys; sys.path.append(x[0:x.rfind('/')])";
+    Global::debug(1) << "Executing '" << python_string.str() << "'" << endl;
+    Global::debug(1) << "Python: " << PyRun_SimpleString(python_string.str().c_str()) << endl;
+    int from = path.rfind("/")+1;
+    int to = path.rfind(".");
+    string module = path.substr(from, to - from);
+    Global::debug(1) << "Loading module " << module << endl;
+    PyObject * python_module = PyImport_ImportModule(module.c_str());
+    Global::debug(1) << "Loaded " << python_module << endl;
+    if (python_module == NULL){
+        PyErr_Print();
+    }
 }
 
 void PythonEngine::init(){
