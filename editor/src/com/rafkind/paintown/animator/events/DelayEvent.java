7b50501b6010fb3f22bb9098c836858acf55abde
Jon Rafkind <jon@rafkind.com>
2008-12-24 18:41:21 +0000

    clean up the delay dialog

--- editor/src/com/rafkind/paintown/animator/events/DelayEvent.java
+++ editor/src/com/rafkind/paintown/animator/events/DelayEvent.java
@@ -11,42 +11,38 @@ import com.rafkind.paintown.animator.events.AnimationEvent;
 import org.swixml.SwingEngine;
 
 public class DelayEvent implements AnimationEvent {
-	private int _delay;
-	
-	public void loadToken(Token token){
-		_delay = token.readInt(0);
-	}
-	
-	public void interact( Animation animation ){
-		animation.setDelay( _delay );
-	}
-	
-	public String getName(){
-		return getToken().toString();
-	}
-	
-	public JPanel getEditor( Animation animation ){
-		SwingEngine engine = new SwingEngine( "animator/eventdelay.xml" );
-		((JPanel)engine.getRootComponent()).setSize(200,50);
-		
-		final JSpinner delayspin = (JSpinner) engine.find( "delay" );
-		delayspin.setValue(new Integer(_delay));
-		delayspin.addChangeListener( new ChangeListener()
-		{
-			public void stateChanged(ChangeEvent changeEvent)
-			{
-				_delay = ((Integer)delayspin.getValue()).intValue();
-			}
-		});
-		return (JPanel)engine.getRootComponent();
-	}
-	
-	public Token getToken()
-	{
-		Token temp = new Token("delay");
-		temp.addToken(new Token("delay"));
-		temp.addToken(new Token(Integer.toString(_delay)));
-		
-		return temp;
-	}
+    private int _delay;
+
+    public void loadToken(Token token){
+        _delay = token.readInt(0);
+    }
+
+    public void interact( Animation animation ){
+        animation.setDelay( _delay );
+    }
+
+    public String getName(){
+        return getToken().toString();
+    }
+
+    public JPanel getEditor( Animation animation ){
+        SwingEngine engine = new SwingEngine( "animator/eventdelay.xml" );
+        ((JPanel)engine.getRootComponent()).setSize(200,100);
+
+        final JSpinner delayspin = (JSpinner) engine.find( "delay" );
+        delayspin.setValue(new Integer(_delay));
+        delayspin.addChangeListener( new ChangeListener(){
+            public void stateChanged(ChangeEvent changeEvent){
+                _delay = ((Integer)delayspin.getValue()).intValue();
+            }
+        });
+        return (JPanel)engine.getRootComponent();
+    }
+
+    public Token getToken(){
+        Token temp = new Token("delay");
+        temp.addToken(new Token("delay"));
+        temp.addToken(new Token(Integer.toString(_delay)));
+        return temp;
+    }
 }
