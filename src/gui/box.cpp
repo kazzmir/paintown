c8295fe77da3373f1f74466ddd30c28dd8c32bad
Jon Rafkind <jon@rafkind.com>
2009-01-03 19:08:47 +0000

    implement copy constructors

--- src/gui/box.cpp
+++ src/gui/box.cpp
@@ -1,50 +1,44 @@
 #include "util/bitmap.h"
 #include "gui/box.h"
 
-Box::Box()
-{
+Box::Box(){
 	// Nothing yet
 }
 
 Box::Box( const Box & b ){
+    this->position = b.position;
+    this->workArea = b.workArea;
 }
 
-Box::~Box()
-{
+Box::~Box(){
 	// Nothing yet
 }
 
 Box &Box::operator=( const Box &copy){
-	position = copy.position;
-	workArea = copy.workArea;
-	
-	return *this;
+    position = copy.position;
+    workArea = copy.workArea;
+
+    return *this;
 }
 
 // Logic
-void Box::logic()
-{
+void Box::logic(){
 	// Nothing yet
 }
 
 // Render
-void Box::render(const Bitmap *work)		
-{
-	checkWorkArea();
-	// Check if we are using a rounded box
-	if(position.radius>0)
-	{
-		roundRectFill( workArea, position.radius, 0, 0, position.width-1, position.height-1, position.body );
-		roundRect( workArea, position.radius, 0, 0, position.width-1, position.height-1, position.border );
-	}
-	else
-	{
-		workArea->rectangleFill( 0, 0, position.width-1, position.height-1, position.body );
-		workArea->rectangle( 0, 0, position.width-1, position.height-1, position.border );
-	}
-	Bitmap::transBlender( 0, 0, 0, position.bodyAlpha );
-	workArea->drawingMode( Bitmap::MODE_TRANS );
-	workArea->drawTrans(position.x,position.y,*work);
-	work->drawingMode( Bitmap::MODE_SOLID );
+void Box::render(const Bitmap *work){
+    checkWorkArea();
+    // Check if we are using a rounded box
+    if(position.radius>0){
+        roundRectFill( workArea, position.radius, 0, 0, position.width-1, position.height-1, position.body );
+        roundRect( workArea, position.radius, 0, 0, position.width-1, position.height-1, position.border );
+    } else {
+        workArea->rectangleFill( 0, 0, position.width-1, position.height-1, position.body );
+        workArea->rectangle( 0, 0, position.width-1, position.height-1, position.border );
+    }
+    Bitmap::transBlender( 0, 0, 0, position.bodyAlpha );
+    workArea->drawingMode( Bitmap::MODE_TRANS );
+    workArea->drawTrans(position.x,position.y,*work);
+    work->drawingMode( Bitmap::MODE_SOLID );
 }
-
--- src/gui/rectarea.cpp
+++ src/gui/rectarea.cpp
@@ -4,41 +4,41 @@ RectArea::RectArea() : x(0), y(0), width(0), height(0), body(0), bodyAlpha(255),
 {
 }
 
-RectArea::RectArea(int x, int y, int w, int h)
-{
-	this->x  = x;
-	this->y  = y;
-	this->width  = width;
-	this->height  = height;
+RectArea::RectArea(int x, int y, int w, int h){
+    this->x  = x;
+    this->y  = y;
+    this->width  = width;
+    this->height  = height;
 }
 
 RectArea::RectArea( const RectArea &r ){
-
+    this->x = r.x;
+    this->y = r.y;
+    this->width = r.width;
+    this->height = r.height;
 }
 
-bool RectArea::empty()
-{
-	return (x==0 && y==0 && width==0 && height==0);
+bool RectArea::empty(){
+    return (x==0 && y==0 && width==0 && height==0);
 }
 
-bool RectArea::operator==( const RectArea &rect)
-{
-	return ( (x == rect.x) &&
-			(y == rect.y) &&
-			(width == rect.width) &&
-			(height == rect.height));
+bool RectArea::operator==( const RectArea &rect){
+    return ( (x == rect.x) &&
+            (y == rect.y) &&
+            (width == rect.width) &&
+            (height == rect.height));
 }
 
 RectArea &RectArea::operator=( const RectArea &copy){
-	x =  copy.x;
-	y = copy.y;
-	width = copy.width; 
-	height =  copy.height;
-	body =  copy.body;
-	bodyAlpha =  copy.bodyAlpha;
-	border = copy.border;
-	borderAlpha = copy.borderAlpha;
-	radius = copy.radius;
-	
-	return *this;
+    x =  copy.x;
+    y = copy.y;
+    width = copy.width; 
+    height =  copy.height;
+    body =  copy.body;
+    bodyAlpha =  copy.bodyAlpha;
+    border = copy.border;
+    borderAlpha = copy.borderAlpha;
+    radius = copy.radius;
+
+    return *this;
 }
