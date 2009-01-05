465c60705bceb210543e2e94f8337c8d0ef5a7a5
Jon Rafkind <jon@rafkind.com>
2009-01-05 07:54:14 +0000

    add header file

---
+++ src/script/object.h
@@ -0,0 +1,34 @@
+#ifndef _paintown_script_object_h
+#define _paintown_script_object_h
+
+class Object;
+namespace Script{
+
+    /* mirrors an enemy in the game */
+    class Object{
+    public:
+        Object(::Object * const guy);
+        virtual ~Object();
+
+        virtual void takeDamage(Object * him, int damage);
+        virtual void tick();
+
+        virtual inline ::Object* const getObject(){
+            return guy;
+        }
+
+    protected:
+        virtual inline void * getHandle(){
+            return handle;
+        }
+
+    protected:
+        /* opaque object that the engine used to communicate between
+         * the paintown engine and the script engine
+         */
+        void * handle;
+        ::Object * const guy;
+    };
+}
+
+#endif
