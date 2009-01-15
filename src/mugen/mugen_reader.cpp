6175811286ee4bfd2d86b3217d6a337c3f134d00
juvinious <juvinious@gmail.com>
2009-01-15 23:57:44 +0000

    Removed code to parse expressions in order to pass to lex/yacc handler

--- src/mugen/mugen_reader.cpp
+++ src/mugen/mugen_reader.cpp
@@ -41,20 +41,11 @@ const std::vector< MugenSection * > & MugenReader::getCollection() throw(MugenEx
   
   const char openbracket = '[';
   const char closebracket = ']';
-  const char openparen = '(';
-  const char closeparen = ')';
   const char comment = ';';
   const char seperator = ',';
   const char colon = ':';
   const char equal = '=';
-  const char notequal = '!';
   const char quote = '"';
-  const char orcondition = '|';
-  const char andcondition = '&';
-  const char greater = '>';
-  const char less = '<';
-  const char add = '+';
-  const char multiply = '*';
   
   
   SearchState state = Section;
@@ -62,7 +53,6 @@ const std::vector< MugenSection * > & MugenReader::getCollection() throw(MugenEx
   MugenSection * sectionHolder = 0;
   // Marker for areas
   bool beginSection = false;
-  bool inQuote = false;
       
   while( !ifile.eof() ){
       getline( ifile, line );
@@ -72,6 +62,8 @@ const std::vector< MugenSection * > & MugenReader::getCollection() throw(MugenEx
       MugenItemContent *itemHolder = new MugenItemContent();
       // Needed to kill loop
       bool breakLoop = false;
+      bool inQuote = false;
+      bool getExpression = false;
       
       for( unsigned int i = 0; i < line.size(); ++i ){
 	    // Go to work
@@ -121,8 +113,12 @@ const std::vector< MugenSection * > & MugenReader::getCollection() throw(MugenEx
 		    break;
 		}
 		// Buh bye spaces
-		if ( line[i] == ' ' && !inQuote ){
+		if( line[i] == ' ' && !inQuote ){
 		  continue;
+		}
+		// Start getting expression
+		if( line[i] != ' ' && getExpression ){
+		  inQuote = true;
 		}
 		// Check if this section is done, push it on the stack and reset everything
 		if( line[i] == openbracket && !checkClsn( contentHolder ) && !itemHolder->hasItems() ){
@@ -132,136 +128,15 @@ const std::vector< MugenSection * > & MugenReader::getCollection() throw(MugenEx
 		  contentHolder = "";
 		  break;
 		}
-		// It's peanut butter bracket time
-		else if( line[i] == openbracket && !checkClsn( contentHolder ) && !inQuote ){
-		  if( !contentHolder.empty() ) *itemHolder << contentHolder;
-		  contentHolder = openbracket;
-		  *itemHolder << contentHolder;
-		  contentHolder = "";
-		}
-		else if( line[i] == closebracket && !checkClsn( contentHolder )  && !inQuote ){
-		  if( !contentHolder.empty() ) *itemHolder << contentHolder;
-		  contentHolder = closebracket;
-		  *itemHolder << contentHolder;
-		  contentHolder = "";
-		}
 		// We got one push back the other and reset the holder to get the next
 		else if( line[i] == colon || line[i] == seperator ){
 		  if( !contentHolder.empty() ) *itemHolder << contentHolder;
 		  contentHolder = "";
 		}
-		// Conditional or
-		else if( line[i] == orcondition && !inQuote ){
-		  // Check to see if the next line is another |
-		  if( i+1 != line.size() ){
-		    if( line[i+1] == orcondition ){
-			if( !contentHolder.empty() ) *itemHolder << contentHolder;
-			contentHolder = orcondition;
-		    }
-		    else {
-		      contentHolder += orcondition;
-		      *itemHolder << contentHolder;
-		      contentHolder = "";
-		    }
-		  }
-		}
-		// Conditional and
-		else if( line[i] == andcondition && !inQuote ){
-		  // Check to see if the next line is another &
-		  if( i+1 != line.size() ){
-		    if( line[i+1] == andcondition ){
-			if( !contentHolder.empty() ) *itemHolder << contentHolder;
-			contentHolder = andcondition;
-		    }
-		    else {
-		      contentHolder += andcondition;
-		      *itemHolder << contentHolder;
-		      contentHolder = "";
-		    }
-		  }
-		}
-		// Parenthesis open
-		else if(line[i] == openparen && !inQuote ){
-		  if( !contentHolder.empty() ) *itemHolder << contentHolder;
-		  contentHolder = openparen;
-		  *itemHolder << contentHolder;
-		  contentHolder = "";
-		}
-		// Parenthesis closed
-		else if(line[i] == closeparen && !inQuote ){
-		  if( !contentHolder.empty() ) *itemHolder << contentHolder;
-		  contentHolder = closeparen;
-		  *itemHolder << contentHolder;
-		  contentHolder = "";
-		}
 		// Equal
 		else if(line[i] == equal && !inQuote ){
 		  if( !contentHolder.empty() ) *itemHolder << contentHolder;
-		  contentHolder = equal;
-		  *itemHolder << contentHolder;
-		  contentHolder = "";
-		}
-		// Not Equal
-		else if( line[i] == notequal && !inQuote ){
-		  // Check to see if the next line is =
-		  if( i+1 != line.size() ){
-		    if( line[i+1] == equal ){
-			if( !contentHolder.empty() ) *itemHolder << contentHolder;
-			contentHolder = "!=";
-			*itemHolder << contentHolder;
-			contentHolder = "";
-			i++;
-		    }
-		  }
-		}
-		// Greater than
-		else if(line[i] == greater && !inQuote ){
-		   // Check to see if the next line is =
-		  if( i+1 != line.size() ){
-		    if( line[i+1] == equal ){
-			if( !contentHolder.empty() ) *itemHolder << contentHolder;
-			contentHolder = ">=";
-			*itemHolder << contentHolder;
-			contentHolder = "";
-			i++;
-		    } else{
-			if( !contentHolder.empty() ) *itemHolder << contentHolder;
-			contentHolder = greater;
-			*itemHolder << contentHolder;
-			contentHolder = "";
-		    }
-		  } 
-		}
-		// Less than
-		else if(line[i] == less && !inQuote ){
-		   // Check to see if the next line is =
-		  if( i+1 != line.size() ){
-		    if( line[i+1] == equal ){
-			if( !contentHolder.empty() ) *itemHolder << contentHolder;
-			contentHolder = "<=";
-			*itemHolder << contentHolder;
-			contentHolder = "";
-			i++;
-		    } else{
-			if( !contentHolder.empty() ) *itemHolder << contentHolder;
-			contentHolder = less;
-			*itemHolder << contentHolder;
-			contentHolder = "";
-		    }
-		  } 
-		}
-		// Add
-		else if(line[i] == add && !inQuote ){
-		  if( !contentHolder.empty() ) *itemHolder << contentHolder;
-		  contentHolder = add;
-		  *itemHolder << contentHolder;
-		  contentHolder = "";
-		}
-		// Multiply
-		else if(line[i] == multiply && !inQuote ){
-		  if( !contentHolder.empty() ) *itemHolder << contentHolder;
-		  contentHolder = multiply;
-		  *itemHolder << contentHolder;
+		  getExpression = true;
 		  contentHolder = "";
 		}
 		// We has a quote begin or end it
