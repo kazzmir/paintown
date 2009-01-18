16c6af8a1053b8b1fd8764f46d792b052e2612b2
juvinious <juvinious@gmail.com>
2009-01-18 16:28:44 +0000

    Added more data loading for MugenCharacter

--- src/mugen/mugen_character.cpp
+++ src/mugen/mugen_character.cpp
@@ -32,9 +32,9 @@ MugenCharacter::~MugenCharacter(){
 
 void MugenCharacter::load() throw( MugenException ){
     // Lets look for our def since some assholes think that all file systems are case insensitive
-    std::string loc = Util::getDataPath() + "mugen/chars/" + location + "/";
-    cout << loc << endl;
-    std::vector< string > files = Util::getFiles( loc, "*" );
+    std::string baseDir = Util::getDataPath() + "mugen/chars/" + location + "/";
+    cout << baseDir << endl;
+    std::vector< string > files = Util::getFiles( baseDir, "*" );
     std::string ourDefFile = "";
     for( unsigned int i = 0; i < files.size(); ++i ){
 	std::string temp = files[i].c_str();
@@ -47,16 +47,11 @@ void MugenCharacter::load() throw( MugenException ){
     }
     if( ourDefFile.empty() )throw MugenException( "Cannot locate player definition file for: " + location );
     
-    cout << loc << ourDefFile << endl;
+    cout << baseDir << ourDefFile << endl;
     
-    MugenReader reader( loc + ourDefFile );
+    MugenReader reader( baseDir + ourDefFile );
     std::vector< MugenSection * > collection;
-    try{
-	collection = reader.getCollection();
-    }
-    catch( MugenException &ex){
-	throw ex;
-    }
+    collection = reader.getCollection();
     
     /* Extract info for our first section of our character */
     for( unsigned int i = 0; i < collection.size(); ++i ){
@@ -82,9 +77,101 @@ void MugenCharacter::load() throw( MugenException ){
 		    *content->getNext() >> author;
 		}
 		else if (itemhead == "pal.defaults" ){
+		    // Add parse info
 		}
-		else throw MugenException( "Unhandled option in Info Section" );
+		else throw MugenException( "Unhandled option in Info Section: " + itemhead );
+	    }
+	}
+	else if( head == "Files" ){
+	    while( collection[i]->hasItems() ){
+		MugenItemContent *content = collection[i]->getNext();
+		const MugenItem *item = content->getNext();
+		const std::string &itemhead = item->query();
+		if( itemhead == "cmd" ){
+		    *content->getNext() >> cmdFile;
+		}
+		else if (itemhead == "cns" ){
+		    *content->getNext() >> constantsFile;
+		}
+		else if (itemhead == "st" ){
+		    *content->getNext() >> stateFile;
+		}
+		else if (itemhead == "stcommon" ){
+		    *content->getNext() >> commonStateFile;
+		}
+		else if (itemhead == "sprite" ){
+		    *content->getNext() >> sffFile;
+		}
+		else if (itemhead == "anim" ){
+		    *content->getNext() >> airFile;
+		}
+		else if (itemhead == "sound" ){
+		    *content->getNext() >> sndFile;
+		}
+		else if (itemhead == "pal1" ){
+		    *content->getNext() >> palFile[0];
+		}
+		else if (itemhead == "pal2" ){
+		    *content->getNext() >> palFile[1];
+		}
+		else if (itemhead == "pal3" ){
+		    *content->getNext() >> palFile[2];
+		}
+		else if (itemhead == "pal4" ){
+		    *content->getNext() >> palFile[3];
+		}
+		else if (itemhead == "pal5" ){
+		    *content->getNext() >> palFile[4];
+		}
+		else if (itemhead == "pal6" ){
+		    *content->getNext() >> palFile[5];
+		}
+		else if (itemhead == "pal7" ){
+		    *content->getNext() >> palFile[6];
+		}
+		else if (itemhead == "pal8" ){
+		    *content->getNext() >> palFile[7];
+		}
+		else if (itemhead == "pal9" ){
+		    *content->getNext() >> palFile[8];
+		}
+		else if (itemhead == "pal10" ){
+		    *content->getNext() >> palFile[9];
+		}
+		else if (itemhead == "pal11" ){
+		    *content->getNext() >> palFile[10];
+		}
+		else if (itemhead == "pal12" ){
+		    *content->getNext() >> palFile[11];
+		}
+		else throw MugenException( "Unhandled option in Files Section: " + itemhead );
+	    }
+	}
+	else if( head == "Arcade" ){
+	    while( collection[i]->hasItems() ){
+		MugenItemContent *content = collection[i]->getNext();
+		const MugenItem *item = content->getNext();
+		const std::string &itemhead = item->query();
+		if( itemhead == "intro.storyboard" ){
+		    *content->getNext() >> introFile;
+		}
+		else if (itemhead == "ending.storyboard" ){
+		    *content->getNext() >> endingFile;
+		}
+		else throw MugenException( "Unhandled option in Info Section: " + itemhead );
 	    }
 	}
     }
+    
+    /* Sprites */
+    MugenSffReader spriteReader( baseDir + sffFile );
+    sprites = spriteReader.getCollection();
+    
+    /* Animations */
+    // Need to build the air reader
+    
+    /* Sounds */
+    MugenSndReader soundReader( baseDir + sndFile );
+    sounds = soundReader.getCollection();
+    
 }
