5998a7784ebd1aca2da658f8fa7aea7aea153533
juvinious <juvinious@gmail.com>
2009-02-17 06:08:36 +0000

    Fixed typo

--- src/mugen/mugen_stage.cpp
+++ src/mugen/mugen_stage.cpp
@@ -652,7 +652,7 @@ void MugenStage::logic( ){
         /* use local variables more often, iterators can be easily confused */
         Object * player = *it;
         player->act( &p2objects, (World *)this, &add);
-	if(!zoffsetlink)player->setZ( zoffset - cameray );
+	if(zoffsetlink == -2580 )player->setZ( zoffset - cameray );
 	else player->setZ( zoffset );
 	
 	if( isaPlayer( player ) ){
@@ -728,7 +728,7 @@ void MugenStage::logic( ){
     for (vector<Object*>::iterator it = p2objects.begin(); it != p2objects.end(); ++it){
 	Object * player = *it;
         player->act( &p1objects, (World *)this, &add);
-	if(!zoffsetlink)player->setZ( zoffset - cameray );
+	if( zoffsetlink == -2580 )player->setZ( zoffset - cameray );
 	else player->setZ( zoffset );
 	
 	if( isaPlayer( player ) ){
