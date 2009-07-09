35221caec5207d9d0051cd776ade373124a59364
Jon Rafkind <jon@rafkind.com>
2009-07-09 05:22:07 +0000

    comments

--- src/environment/atmosphere.h
+++ src/environment/atmosphere.h
@@ -6,15 +6,23 @@ class Bitmap;
 #include <string>
 
 class Atmosphere{
+protected:
+	Atmosphere();
 public:
-
-	Atmosphere();	
 	virtual ~Atmosphere();
 
+        /* drawn in front of all background things but before objects */
 	virtual void drawBackground(Bitmap * work, int x) = 0;
+
+        /* drawn in front of all objects */
 	virtual void drawForeground(Bitmap * work, int x) = 0;
+        
+        /* drawn only on the foreground */
 	virtual void drawFront(Bitmap * work, int x) = 0;
+
+        /* drawn on the entire screen */
         virtual void drawScreen(Bitmap * work, int x) = 0;
+
 	virtual void act() = 0;
 
 	static Atmosphere * createAtmosphere( const std::string & name );
