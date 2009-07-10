ba8a093fc995f11a164e5b10500c75f8fe3c48c6
Jon Rafkind <jon@rafkind.com>
2009-07-10 20:18:47 +0000

    dont assume first token is "name"

--- src/menu/option_menu.cpp
+++ src/menu/option_menu.cpp
@@ -5,12 +5,13 @@
 #include "return_exception.h"
 #include "util/token_exception.h"
 #include "util/funcs.h"
+#include "globals.h"
 
-OptionMenu::OptionMenu(Token *token) throw (LoadException): MenuOption(token, Event), _menu(0)
-{
+OptionMenu::OptionMenu(Token *token) throw (LoadException):
+MenuOption(token, Event),
+_menu(0){
     // Check whether we have a menu or tabmenu
-    if ( *token == "menu" )
-    {
+    if ( *token == "menu" ){
 	_menu = new Menu();
     } else if (*token == "tabmenu"){
 	_menu = new TabMenu();
@@ -20,16 +21,21 @@ OptionMenu::OptionMenu(Token *token) throw (LoadException): MenuOption(token, Ev
     // Set this menu as an option
     _menu->setAsOption(true);
     
+    /*
     // Lets try loading from a file
     std::string temp;
     // Filename
     *token >> temp;
-    if(temp=="name")
-    {
-	    token->resetToken();
-	    _menu->load(token);
+    */
+
+    if (token->numTokens() == 1){
+        std::string temp;
+        *token >> temp;
+        _menu->load(Util::getDataPath() + temp);
+    } else {
+        _menu->load(token);
     }
-    else _menu->load(Util::getDataPath() + temp);
+
     this->setText(_menu->getName());
     
     // Lets check if this menu is going bye bye
