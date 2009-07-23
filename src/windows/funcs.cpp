bbefb26e2ef1b7f10972346bef7b160827f3e207
Jon Rafkind <jon@rafkind.com>
2009-07-23 18:16:04 +0000

    fix pipe in windows

--- src/windows/funcs.cpp
+++ src/windows/funcs.cpp
@@ -1,16 +1,20 @@
 #ifdef WINDOWS
 
+#include <windows.h>
 #include "util/funcs.h"
 #include <io.h>
+#include <fcntl.h>
 
 int Util::getPipe(int files[2]){
-    PHANDLE read_in, write_in;
-    int ok = CreatePipe(read_in, write_in, NULL, 0);
+    HANDLE read_in, write_in;
+    int ok = CreatePipe(&read_in, &write_in, NULL, 0);
     if (ok == 0){
         return -1;
     }
-    files[0] = _open_osfhandle(read_in, O_RDONLY | O_BINARY);
-    files[1] = _open_osfhandle(write_in, O_WRONLY | O_BINARY);
+    // files[0] = _open_osfhandle(read_in, O_RDONLY | O_BINARY);
+    // files[1] = _open_osfhandle(write_in, O_WRONLY | O_BINARY);
+    files[0] = _open_osfhandle((intptr_t) read_in, _O_RDONLY);
+    files[1] = _open_osfhandle((intptr_t) write_in, _O_WRONLY);
     return 0;
 }
 
