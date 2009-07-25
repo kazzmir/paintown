d9bdaa85fc4f2b80d03c79a9b64819f5942887f2
Jon Rafkind <jon@rafkind.com>
2009-07-25 05:59:16 +0000

    check for directory on windows

--- src/windows/system.cpp
+++ src/windows/system.cpp
@@ -1,11 +1,17 @@
 #ifdef WINDOWS
 
+#include <windows.h>
+
 #include "util/system.h"
 
 namespace System{
 
+static bool isDirectory(const std::string & path){
+    return GetFileAttributes(path.c_str()) & FILE_ATTRIBUTE_DIRECTORY;
+}
+
 bool readableFile(const std::string & path){
-    return true;
+    return !isDirectory(path);
 }
 
 }
