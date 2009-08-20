67aaf915a4892534ac94f9cfebdea20121278264
Jon Rafkind <jon@rafkind.com>
2009-08-20 04:01:42 +0000

    start a trigger class

---
+++ src/trigger/trigger.h
@@ -0,0 +1,28 @@
+#ifndef _paintown_trigger_h
+#define _paintown_trigger_h
+
+/* design goals of a trigger
+ * 1. can contain arbitrary code (possibly scheme since we are
+ * already using s-expressions)
+ *   (trigger (scheme s-expr))
+ *   or
+ *   (trigger (python file.py))
+ *   or
+ *   (trigger add-light (x 50) (y 100) (strength 20))
+ * 2. fired based on arbitrary constraints
+ *   (trigger (check blah))
+ */
+class Trigger{
+public:
+    Trigger();
+
+    /* do something */
+    void execute() = 0;
+
+    /* whether or not this trigger should execute */
+    bool shouldExecute() const = 0;
+
+    virtual ~Trigger();
+};
+
+#endif
