999903c99efabc21618018a4ee70d2c187ac99aa
Jon Rafkind <jon@rafkind.com>
2009-10-07 21:28:14 +0000

    add stub script to build in a windows vm

---
+++ misc/build-windows.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# start up windows vm
+# some script should start on bootup and try to connect back to this script (through some port)
+# this script should send commands for building the tree and creating the exe
+# could also have a simple file transfer mechanism so I can get the exe without having to muck around with ftp or whatever
+
+def start_windows_vm():
+    pass
+
+# returns a connection
+def wait_for_connect():
+    pass
+
+# gets the text output from sending commands
+def send_build_commands(connection):
+    pass
