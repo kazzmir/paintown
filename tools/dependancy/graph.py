5250c64510e2b91dfda776971864a98665933a15
Jon Rafkind <jon@rafkind.com>
2009-10-20 03:41:50 +0000

    mostly works

--- tools/dependancy/graph.py
+++ tools/dependancy/graph.py
@@ -2,19 +2,82 @@
 
 # dependancy will just be a map from a file to a list of files
 
+# true if the directory contains a .svn in it
+def svn(dir):
+    return '.svn' in dir
+
+def read_includes(file):
+    import re
+    input = open(file)
+    local_include = re.compile('^#include\s+"(.*)"')
+    system_include = re.compile('^#include\s+<(.*)>')
+    for line in input.readlines():
+        out = local_include.match(line)
+        if out != None:
+            yield out.group(1)
+        else:
+            out = system_include.match(line)
+            if out != None:
+                yield out.group(1)
+        
+    input.close()
+
+def read_dependancies(file, root):
+    out = []
+    for include in read_includes(file):
+        import os
+        if os.path.exists(include):
+            out.append(include)
+        elif os.path.exists(root + '/' + include):
+            out.append(os.path.normpath('%s/%s' % (root, include)))
+        else:
+            out.append(include)
+    return out
+
 def find_dependancies(dir):
     import os
-    return None
+
+    def source(name):
+        import re
+        c_file = re.compile('.*\.c$')
+        cpp_file = re.compile('.*\.cpp$')
+        h_file = re.compile('.*\.h$')
+        return c_file.match(name) != None or\
+               cpp_file.match(name) != None or\
+               h_file.match(name) != None
+
+    out = {}
+    for root, dirs, files in os.walk(dir):
+        if not svn(root):
+            for file in [f for f in files if source(f)]:
+                print "%s %s" % (root, file)
+                out[os.path.normpath('%s/%s' % (root,file))] = read_dependancies('%s/%s' % (root, file), root)
+
+    return out
 
 def generate_graph(dependancies):
 
     data = []
 
-    def addLink(from_file, to_file):
-        data.append("%s -> %s;" % (from_file, to_file))
+    names = {}
+    next_name = [0]
+    def dot_name(name):
+        try:
+            return names[name]
+        except KeyError:
+            next_name[0] = next_name[0] + 1
+            names[name] = 's%s' % next_name[0]
+            return names[name]
 
-    addLink('a', 'b')
-    addLink('b', 'c')
+    def addLink(from_file, to_file):
+        # memoize these names so they aren't repeatedly added
+        data.append('%s [label = "%s"]' % (dot_name(from_file), from_file))
+        data.append('%s [label = "%s"]' % (dot_name(to_file), to_file))
+        data.append("%s -> %s;" % (dot_name(from_file), dot_name(to_file)))
+
+    for key, value in dependancies.items():
+        for to in value:
+            addLink(key, to)
 
     return """
 digraph X {
@@ -22,9 +85,12 @@ digraph X {
 }
 """ % '\n'.join(data)
 
+# generates a ps file
 def run_dot(file):
     import subprocess
-    subprocess.call(['dot', '-Tps', file, '-o', '%s.ps' % file])
+    out = "%s.ps" % file
+    subprocess.call(['dot', '-Tps', file, '-o', out])
+    print "Generated %s" % out
 
 def generate(dir, out):
     dependancies = find_dependancies(dir)
