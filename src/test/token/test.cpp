dfe59ab6ac0eaba1a6b22191ad555f3469dcff15
Jon Rafkind <jon@rafkind.com>
2009-11-21 22:25:58 +0000

    add another token test

--- src/test/token/test.cpp
+++ src/test/token/test.cpp
@@ -58,10 +58,28 @@ static void test2(){
     }
 }
 
+static void test3_write(string file){
+    ofstream out(file.c_str());
+    out << "(foo1 (foo2 (foo3)) (foo2 (foo3)))";
+    out.close();
+}
+
+static void test3(){
+    string file = randomFile();
+    test3_write(file);
+    TokenReader reader(file);
+    Token * head = reader.readToken();
+    vector<Token*> tokens = head->findTokens("foo1/foo2/foo3");
+    if (tokens.size() != 2){
+        throw Failure(3);
+    }
+}
+
 int main(){
     try{
         test1();
         test2();
+        test3();
         cout << "All tests passed!" << endl;
     } catch (const Failure & f){
         cout << "Test case " << f.num << " failed" << endl;
