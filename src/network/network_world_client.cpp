06c65c593c4183b5b777b384e6b1bc4e48036b14
Jon Rafkind <jon@rafkind.com>
2009-12-24 23:24:28 +0000

    make ping number larger

--- src/network/network_world_client.cpp
+++ src/network/network_world_client.cpp
@@ -490,7 +490,7 @@ void NetworkWorldClient::sendMessages(const vector<Network::Message> & messages,
 	
 void NetworkWorldClient::draw(Bitmap * work){
     super::draw(work);
-    const Font & font = Font::getFont(Filesystem::find(Global::DEFAULT_FONT), 9, 9);
+    const Font & font = Font::getFont(Filesystem::find(Global::DEFAULT_FONT), 15, 15);
     FontRender * render = FontRender::getInstance();
 
     /* the coordinates are only right becuase I know that the screen is twice as
@@ -498,7 +498,7 @@ void NetworkWorldClient::draw(Bitmap * work){
      * its coordinate system, something like opengl where -1 is the top and 1
      * is the bottom.
      */
-    render->addMessage(font, 1, work->getHeight() * 2 - 11, Bitmap::makeColor(255, 255, 255), -1, "Ping %d", (int) (currentPing / 1000));
+    render->addMessage(font, 1, work->getHeight() * 2 - font.getHeight() - 1, Bitmap::makeColor(255, 255, 255), -1, "Ping %d", (int) (currentPing / 1000));
     // font.printf(1, work->getHeight() - 11, Bitmap::makeColor( 255, 255, 255 ), *work, "Ping %d", 0, (int) (currentPing / 1000));
 }
 
