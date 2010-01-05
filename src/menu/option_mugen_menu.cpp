b0862285d70fcc002ecf02fd4dd3b6fb36ef77fd
Jon Rafkind <jon@rafkind.com>
2010-01-05 03:52:19 +0000

    use normal loading screen when loading mugen

--- src/menu/option_mugen_menu.cpp
+++ src/menu/option_mugen_menu.cpp
@@ -7,6 +7,8 @@
 #include "util/token_exception.h"
 #include "util/funcs.h"
 #include "globals.h"
+#include "loading.h"
+#include <pthread.h>
 
 #include "mugen/mugen_menu.h"
 #include "mugen/mugen_exception.h"
@@ -14,73 +16,77 @@
 
 using namespace std;
 
-OptionMugenMenu::OptionMugenMenu(Token *token) throw (LoadException): MenuOption(token, Event), _menu(0)
-{
-	if ( *token != "mugen" ){
-		throw LoadException("Not a mugen motif menu");
-	}
-	while ( token->hasTokens() ){
-		try{
-			Token * tok;
-			*token >> tok;
-			if ( *tok == "name" ){
-				// Create an image and push it back on to vector
-				std::string temp;
-				*tok >> temp;
-				this->setText(temp);
-			} else if (*tok == "motif"){
-			    // Load Motif from file
-			    std::string temp;
-			    // Filename
-			    *tok >> temp;
-			    _menu = new MugenMenu(temp);
-			}else {
-				Global::debug( 3 ) <<"Unhandled menu attribute: "<<endl;
-                                if (Global::getDebug() >= 3){
-                                    tok->print(" ");
-                                }
-			}
-		} catch ( const TokenException & ex ) {
-			// delete current;
-			string m( "Menu parse error: " );
-			m += ex.getReason();
-			throw LoadException( m );
-		} 
-	}
-	// Set this menu as an option
-	_menu->setAsOption(true);
-	
-	// Lets check if this menu is going bye bye
-	//if ( _menu->checkRemoval() ) setForRemoval(true);
+OptionMugenMenu::OptionMugenMenu(Token *token) throw (LoadException): MenuOption(token, Event), _menu(0){
+    if ( *token != "mugen" ){
+        throw LoadException("Not a mugen motif menu");
+    }
+    while ( token->hasTokens() ){
+        try{
+            Token * tok;
+            *token >> tok;
+            if ( *tok == "name" ){
+                // Create an image and push it back on to vector
+                std::string temp;
+                *tok >> temp;
+                this->setText(temp);
+            } else if (*tok == "motif"){
+                // Load Motif from file
+                std::string temp;
+                // Filename
+                *tok >> temp;
+                _menu = new MugenMenu(temp);
+            }else {
+                Global::debug( 3 ) <<"Unhandled menu attribute: "<<endl;
+                if (Global::getDebug() >= 3){
+                    tok->print(" ");
+                }
+            }
+        } catch ( const TokenException & ex ) {
+            // delete current;
+            string m( "Menu parse error: " );
+            m += ex.getReason();
+            throw LoadException( m );
+        } 
+    }
+    // Set this menu as an option
+    _menu->setAsOption(true);
+
+    // Lets check if this menu is going bye bye
+    //if ( _menu->checkRemoval() ) setForRemoval(true);
 }
 
-OptionMugenMenu::~OptionMugenMenu()
-{
-	// Delete our menu
-	if(_menu)delete _menu;
+OptionMugenMenu::~OptionMugenMenu(){
+    // Delete our menu
+    if (_menu){
+        delete _menu;
+    }
 }
 
-void OptionMugenMenu::logic()
-{
+void OptionMugenMenu::logic(){
 	// Nothing
 }
 
 void OptionMugenMenu::run(bool &endGame){
     // Load er up and throw up a load box to inform the user
-    Box::msgDialog(*getParent()->getWork(),"Loading M.U.G.E.N.!",2);
+    // Box::msgDialog(*getParent()->getWork(),"Loading M.U.G.E.N.!",2);
+    pthread_t loading;
+    Loader::startLoading(&loading);
     try {
-	_menu->loadData();
+        _menu->loadData();
     } catch (const MugenException & ex){
-	string m("Problem with loading MUGEN menu: ");
-	m += ex.getFullReason();
-	throw LoadException(m);
+        string m("Problem with loading MUGEN menu: ");
+        m += ex.getFullReason();
+        Loader::stopLoading(loading);
+        throw LoadException(m);
     }
+    Loader::stopLoading(loading);
+
     try {
-	// Run
-	_menu->run();
+        // Run
+        _menu->run();
     } catch (const ReturnException & re){
-	// Say what?
-	// Do not quit game
+        // Say what?
+        // Do not quit game
     }
 }
 
