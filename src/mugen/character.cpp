61e7bbcaccc585511327c25847ecbf94e86a3fe7
Jon Rafkind <jon@rafkind.com>
2010-01-07 06:02:53 +0000

    implement VelAdd

--- src/mugen/character.cpp
+++ src/mugen/character.cpp
@@ -422,6 +422,18 @@ void StateController::activate(Character & guy) const {
             break;
         }
         case VelAdd : {
+            if (value1 != NULL){
+                RuntimeValue result = evaluate(value1, Environment(guy));
+                if (result.isDouble()){
+                    guy.setXVelocity(guy.getXVelocity() + result.getDoubleValue());
+                }
+            }
+            if (value2 != NULL){
+                RuntimeValue result = evaluate(value2, Environment(guy));
+                if (result.isDouble()){
+                    guy.setYVelocity(guy.getYVelocity() + result.getDoubleValue());
+                }
+            }
             break;
         }
         case VelMul : {
