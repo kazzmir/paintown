cddc8913c50febd93ff1657ba10cdef7f32146bf
Jon Rafkind <jon@rafkind.com>
2010-02-16 21:32:52 +0000

    start to read header

--- tools/sff-reader/src/sffreader.ml
+++ tools/sff-reader/src/sffreader.ml
@@ -1,11 +1,11 @@
+(* Sffreader for mugen .sff files *)
+
 (*
 open ExtLib
 *)
 
-(* Reads bytes in little-endian format *)
 let read_bytes bytes input =
   let read byte = input_byte input in
-  (* little endian, so reverse the numbers *)
   let bytes = List.rev (ExtList.List.of_enum (Enum.map read (ExtList.List.enum
   (ExtList.List.make
   bytes 0)))) in
@@ -13,27 +13,32 @@ let read_bytes bytes input =
   (* Add all the numbers up. For a 4 byte number it would be:
    *   n4 * 256^3 + n3 * 256^2 + n2 * 256^1 + n1 * 256^0
    *)
-  List.fold_left (fun total now -> total * 256 + now) 0 bytes
+  let answer = List.fold_left (fun total now -> total * 256 + now) 0 bytes in
+  (*
+  List.iter (fun y -> Printf.printf "%d, " y) bytes;
+  Printf.printf " = %u\n" answer;
+  *)
+  answer
 ;;
 
 (* curried functions *)
 let read_bytes_2 : in_channel -> int = read_bytes 2;;
 let read_bytes_4 : in_channel -> int = read_bytes 4;;
 
-(* Read the LBX header. See above *)
 let read_header input =
-  let f : int = read_bytes_2 input in
-  Printf.printf "Number of files: %d\n" f;
-  Printf.printf "Signature: %d\n" (read_bytes_4 input);
-  Printf.printf "Version: %d\n" (read_bytes_2 input);
-  for i = 0 to f do
-    Printf.printf "Next file is: %x\n" (read_bytes_4 input);
-  done;
+  (* skip first 16 bytes *)
+  seek_in input 16;
+  let totalGroups = read_bytes_4 input in
+  let totalImages = read_bytes_4 input in
+  let suboffset = read_bytes_4 input in
+  let subhead = read_bytes_4 input in
+  let sharedPalette = read_bytes_2 input in
+  Printf.printf "Total groups %u\n" totalGroups;
+  Printf.printf "Total images %u\n" totalImages;
   0
 ;;
 
-(* Read an LBX archive. `file' is the filename *)
-let read_lbx file =
+let read_sff file =
   Printf.printf "Reading file %s\n" file;
   let input = open_in_bin file in
   Printf.printf "Opened file\n";
@@ -41,5 +46,8 @@ let read_lbx file =
   close_in input;
 ;;
 
-Printf.printf "Lbx reader\n";;
-read_lbx Sys.argv.(1);
+if Array.length Sys.argv < 2 then
+  Printf.printf "Give a .sff file as an argument\n"
+else begin
+  read_sff Sys.argv.(1);
+end
