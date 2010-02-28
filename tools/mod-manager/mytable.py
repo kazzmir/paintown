560ec0989e5fd77ab61afde5dd91fa0890fcdce8
Jon Rafkind <jon@rafkind.com>
2010-02-28 23:50:57 +0000

    use custom table class

--- tools/mod-manager/mytable.py
+++ tools/mod-manager/mytable.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 from PyQt4 import QtCore, QtGui, uic
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
@@ -27,27 +24,3 @@ class MyTable(QtGui.QTableWidget):
     def dropEvent(self, event):
         print "Drop event %s" % event.mimeData().urls()
 
-def run(app):
-    # Set the look and feel
-    QtGui.QApplication.setStyle(QtGui.QStyleFactory.create("Cleanlooks"))
-
-    main = uic.loadUi("main.ui")
-
-    #main.setAcceptDrops(True)
-    #main.mods.setAcceptDrops(True)
-    # table = MyTable(main.mods)
-    # main.addWidget(MyTable())
-
-    # Center on screen
-    screen = QtGui.QDesktopWidget().screenGeometry()
-    size = main.geometry()
-    main.move((screen.width()-size.width())/2, (screen.height()-size.height())/2)
-
-    # show widget
-    main.show()
-
-    # Run
-    app.exec_()
-
-import sys
-run(QtGui.QApplication(sys.argv))
