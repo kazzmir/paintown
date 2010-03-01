64e744360b69c01797b22375a751e4a3d5b14a56
Jon Rafkind <jon@rafkind.com>
2010-03-01 00:12:47 +0000

    start to handle drops

--- tools/mod-manager/manager.py
+++ tools/mod-manager/manager.py
@@ -5,28 +5,6 @@ from PyQt4 import QtCore, QtGui, uic
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
-class MyTable(QtGui.QTableWidget):
-    def __init__(self, parent):
-        QtGui.QTableWidget.__init__(self, parent)
-        self.setAcceptDrops(True)
-
-    def dragEnterEvent(self, event):
-        print "Has urls? %s" % event.mimeData().hasUrls()
-        if event.mimeData().hasUrls():
-            print "Accept event"
-            event.accept()
-        else:
-            event.ignore()
-        # print "Drag event: mime %s" % event.mimeData().urls()
-
-    def dragMoveEvent(self, event):
-        event.accept()
-        pass
-        # print "Drag move event"
-
-    def dropEvent(self, event):
-        print "Drop event %s" % event.mimeData().urls()
-
 def run(app):
     # Set the look and feel
     QtGui.QApplication.setStyle(QtGui.QStyleFactory.create("Cleanlooks"))
--- tools/mod-manager/mytable.py
+++ tools/mod-manager/mytable.py
@@ -2,15 +2,22 @@ from PyQt4 import QtCore, QtGui, uic
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
+def isZipFile(path):
+    import zipfile
+    return zipfile.is_zipfile(path)
+
+def showError(error):
+    print error
+
 class MyTable(QtGui.QTableWidget):
     def __init__(self, parent):
         QtGui.QTableWidget.__init__(self, parent)
         self.setAcceptDrops(True)
 
     def dragEnterEvent(self, event):
-        print "Has urls? %s" % event.mimeData().hasUrls()
+        # print "Has urls? %s" % event.mimeData().hasUrls()
         if event.mimeData().hasUrls():
-            print "Accept event"
+            # print "Accept event"
             event.accept()
         else:
             event.ignore()
@@ -18,9 +25,16 @@ class MyTable(QtGui.QTableWidget):
 
     def dragMoveEvent(self, event):
         event.accept()
-        pass
         # print "Drag move event"
 
-    def dropEvent(self, event):
-        print "Drop event %s" % event.mimeData().urls()
+    def doFile(self, path):
+        if isZipFile(path):
+            print "Installing mod %s" % path
 
+    def dropEvent(self, event):
+        for file in event.mimeData().urls():
+            path = file.path()
+            if isZipFile(path):
+                self.doFile(path)
+            else:
+                showError("'%s' is not a zip file. Please drag and drop .zip files" % path)
