9f6c7d27babac234f24d942ab404de69863e632a
Jon Rafkind <jon@rafkind.com>
2010-03-02 07:20:59 +0000

    add build scripts to create exe

---
+++ tools/mod-manager/make.bat
@@ -0,0 +1,2 @@
+python setup.py py2exe --includes sip,ctypes,ctypes.wintypes,mytable
+cp main.ui dist/.
---
+++ tools/mod-manager/setup.py
@@ -0,0 +1,4 @@
+from py2exe.build_exe import py2exe
+from distutils.core import setup
+setup( console=[{"script": "manager.py"}] )
+
