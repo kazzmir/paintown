fe43d0373e455fd8821d7e1d8dd4c1ab9b066272
Jon Rafkind <jon@rafkind.com>
2010-03-31 23:07:49 +0000

    allow user to specify the character to load

--- src/test/mugen/load.cpp
+++ src/test/mugen/load.cpp
@@ -9,23 +9,35 @@
 
 using namespace std;
 
+static void load(const char * path){
+    try{
+        TimeDifference diff;
+        diff.startTime();
+        Global::debug(0) << "Loading " << path << endl;
+        Mugen::Character kfm(Filesystem::find(Filesystem::RelativePath(path)));
+        kfm.load();
+        diff.endTime();
+        Global::debug(0, "test") << "Success! Took " << diff.printTime() << endl;
+    } catch (const MugenException & e){
+        Global::debug(0, "test") << "Test failure!: " << e.getReason() << endl;
+    } catch (const Filesystem::NotFound & e){
+        Global::debug(0, "test") << "Test failure! Couldn't find a file: " << e.getReason() << endl;
+    }
+}
+
 int main(int argc, char ** argv){
     install_allegro(SYSTEM_NONE, &errno, atexit);
     set_color_depth(16);
     set_color_conversion(COLORCONV_NONE);
     Mugen::ParseCache cache;
 
+    if (argc < 2){
+        load("mugen/chars/kfm/kfm.def");
+    } else {
+        load(argv[1]);
+    }
+
     // for (int i = 0; i < 3; i++){
-        try{
-            TimeDifference diff;
-            diff.startTime();
-            Mugen::Character kfm(Filesystem::find(Filesystem::RelativePath("mugen/chars/kfm/kfm.def")));
-            kfm.load();
-            diff.endTime();
-            Global::debug(0, "test") << "Success! Took " << diff.printTime() << endl;
-        } catch (const MugenException & e){
-            Global::debug(0, "test") << "Test failure!: " << e.getReason() << endl;
-        }
-    // }
+      // }
 }
 END_OF_MAIN()
