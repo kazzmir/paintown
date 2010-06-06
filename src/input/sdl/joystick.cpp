f79f6572f049bcfcd56431557192c0d39988aeea
Jon Rafkind <jon@rafkind.com>
2010-06-06 21:58:45 +0000

    read buttons/axis in sdl joystick

--- src/input/sdl/joystick.cpp
+++ src/input/sdl/joystick.cpp
@@ -7,10 +7,45 @@ void SDLJoystick::poll(){
     /* TODO */
 }
 
+static bool read_button(SDL_Joystick * joystick, int button){
+    return SDL_JoystickGetButton(joystick, button);
+}
+
 JoystickInput SDLJoystick::readAll(){
-    /* TODO */
-    JoystickInput ok;
-    return ok;
+    JoystickInput input;
+    if (joystick){
+        int buttons = SDL_JoystickNumButtons(joystick);
+        switch (buttons > 4 ? 4 : buttons){
+            case 4: input.button4 = read_button(joystick, 3);
+            case 3: input.button3 = read_button(joystick, 2);
+            case 2: input.button2 = read_button(joystick, 1);
+            case 1: input.button1 = read_button(joystick, 0);
+            case 0: {
+                break;
+            }
+        }
+    }
+
+    int axis = SDL_JoystickNumAxes(joystick);
+    if (axis > 0){
+        int position = SDL_JoystickGetAxis(joystick, 0);
+        if (position < 0){
+            input.left = true;
+        } else if (position > 0){
+            input.right = true;
+        }
+    }
+
+    if (axis > 1){
+        int position = SDL_JoystickGetAxis(joystick, 1);
+        if (position < 0){
+            input.up = true;
+        } else if (position > 0){
+            input.down = true;
+        }
+    }
+
+    return input;
 }
 
 SDLJoystick::~SDLJoystick(){
