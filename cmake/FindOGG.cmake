48d004f3d40fac998107cddf6d650daf096f9b22
juvinious <juvinious@gmail.com>
2010-06-09 16:55:16 +0000

    Added module to check for vorbis, updated ogg module to be verbose.

--- cmake/FindOGG.cmake
+++ cmake/FindOGG.cmake
@@ -1,68 +1,51 @@
-# Locate OGG
+# - Locate Ogg library
 # This module defines
-# OGG_LIBRARY
-# OGG_FOUND, if false, do not try to link to OGG 
-# OGG_INCLUDE_DIR, where to find the headers
-#
-# $OGGDIR is an environment variable that would
-# correspond to the ./configure --prefix=$OGGDIR
-# used in building OGG.
-#
-# Created by Sukender (Benoit Neil). Based on FindOGG.cmake module.
+#  OGG_LIBRARY, the library to link against
+#  OGG_FOUND, if false, do not try to link to OGG
+#  OGG_INCLUDE_DIR, where to find headers.
+
+IF(OGG_LIBRARY AND OGG_INCLUDE_DIR)
+  # in cache already
+  SET(OGG_FIND_QUIETLY TRUE)
+ENDIF(OGG_LIBRARY AND OGG_INCLUDE_DIR)
+
 
 FIND_PATH(OGG_INCLUDE_DIR
-        NAMES ogg/ogg.h ogg/os_types.h
-        HINTS
-        $ENV{OGGDIR}
-        $ENV{OGG_PATH}
-        PATH_SUFFIXES include
-        PATHS
-        ~/Library/Frameworks
-        /Library/Frameworks
-        /usr/local
-        /usr
-        /sw # Fink
-        /opt/local # DarwinPorts
-        /opt/csw # Blastwave
-        /opt
+  ogg.h
+  PATHS
+  $ENV{OGG_DIR}/include
+  /usr/local/include
+  /usr/include
+  /sw/include
+  /opt/local/include
+  /opt/csw/include
+  /opt/include
+  PATH_SUFFIXES ogg
 )
 
-FIND_LIBRARY(OGG_LIBRARY 
-        ogg
-        HINTS
-        $ENV{OGGDIR}
-        $ENV{OGG_PATH}
-        PATH_SUFFIXES win32/Dynamic_Release lib
-        PATHS
-        ~/Library/Frameworks
-        /Library/Frameworks
-        /usr/local
-        /usr
-        /sw
-        /opt/local
-        /opt/csw
-        /opt
+FIND_LIBRARY(OGG_LIBRARY
+  NAMES ogg libogg
+  PATHS
+  $ENV{OGG_DIR}/lib
+  /usr/local/lib
+  /usr/lib
+  /usr/local/X11R6/lib
+  /usr/X11R6/lib
+  /sw/lib
+  /opt/local/lib
+  /opt/csw/lib
+  /opt/lib
+  /usr/freeware/lib64
 )
 
-FIND_LIBRARY(OGG_LIBRARY_DEBUG 
-        ogg_d
-        HINTS
-        $ENV{OGGDIR}
-        $ENV{OGG_PATH}
-        PATH_SUFFIXES win32/Dynamic_Debug lib
-        PATHS
-        ~/Library/Frameworks
-        /Library/Frameworks
-        /usr/local
-        /usr
-        /sw
-        /opt/local
-        /opt/csw
-        /opt
-)
-
-
-SET(OGG_FOUND "NO")
 IF(OGG_LIBRARY AND OGG_INCLUDE_DIR)
   SET(OGG_FOUND "YES")
+  IF(NOT OGG_FIND_QUIETLY)
+    MESSAGE(STATUS "Found Ogg: ${OGG_LIBRARY}")
+  ENDIF(NOT OGG_FIND_QUIETLY)
+ELSE(OGG_LIBRARY AND OGG_INCLUDE_DIR)
+  IF(NOT OGG_FIND_QUIETLY)
+    MESSAGE(STATUS "Warning: Unable to find Ogg!")
+  ENDIF(NOT OGG_FIND_QUIETLY)
 ENDIF(OGG_LIBRARY AND OGG_INCLUDE_DIR)
+
