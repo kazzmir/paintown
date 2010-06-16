5bd7b5adcd937c0d55d82797439d5ddc7bd67f38
Jon Rafkind <jon@rafkind.com>
2010-06-16 22:11:06 +0000

    only use main if not using sdl main

--- src/xmain.cpp
+++ src/xmain.cpp
@@ -16,6 +16,7 @@ PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER);
 PSP_HEAP_SIZE_MAX();
 #endif
 
+#ifdef USE_SDL
 #ifdef USE_SDL_MAIN
 extern "C" int SDL_main(int argc, char ** argv){
     return paintown_main(argc, argv);
@@ -24,4 +25,5 @@ extern "C" int SDL_main(int argc, char ** argv){
 int main( int argc, char ** argv ){
     return paintown_main(argc, argv);
 }
+#endif
 #endif
