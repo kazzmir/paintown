3d2e8238e75b52ec1225f8f5f281afbbd682479a
juvinious <juvinious@gmail.com>
2010-07-05 18:55:35 +0000

    Add in information for wiimote with attachements.

--- src/input/wii/joystick.cpp
+++ src/input/wii/joystick.cpp
@@ -13,54 +13,110 @@ void WiiJoystick::poll(){
 JoystickInput WiiJoystick::readAll(){
     JoystickInput input;
     // Number passed should be a port 1-4 I believe same with gc pad this can be modified later for multiplayer
-    u32 held = WPAD_ButtonsHeld(0);
     
-    // WIIMOTE for now, we can add in nunchuck and classic later
-    if(held & WPAD_BUTTON_HOME){
-        //input.button7 = true;
-    } else if(held & WPAD_BUTTON_1){
-        input.button1 = true;
-    } else if(held & WPAD_BUTTON_2){
-        input.button2 = true;
-    } else if(held & WPAD_BUTTON_A){
-    } else if(held & WPAD_BUTTON_B){
-        
-    } else if(held & WPAD_BUTTON_MINUS){
-        input.button4 = true;
-    } else if(held & WPAD_BUTTON_PLUS){
-        input.button3 = true;
-    } else if(held & WPAD_BUTTON_UP){
-        input.left = true;
-    } else if(held & WPAD_BUTTON_DOWN){
-        input.right = true;
-    } else if(held & WPAD_BUTTON_LEFT){
-        input.down = true;
-    } else if(held & WPAD_BUTTON_RIGHT){
+    // Wii data
+    WPADData * wpad;
+    wpad = WPAD_Data(0);
+    
+    // Gamecube
+    u16 gcheld = PAD_ButtonsHeld(0);
+    
+    // Wii controller
+    if (wpad->exp.type == WPAD_EXP_CLASSIC){
+        // Classic controller
+        if(wpad->btns_h & WPAD_CLASSIC_BUTTON_UP){
+            input.up = true;
+        }
+        if(wpad->btns_h & WPAD_CLASSIC_BUTTON_DOWN){
+            input.down = true;
+        }
+        if(wpad->btns_h & WPAD_CLASSIC_BUTTON_LEFT){
+            input.left = true;
+        }
+        if(wpad->btns_h & WPAD_CLASSIC_BUTTON_RIGHT){
+            input.right = true;
+        }
+    } else if (wpad->exp.type == WPAD_EXP_NUNCHUK){
+        // With nunchuck
+        if(wpad->exp.nunchuk.js.pos.y >= 0xB0){
+            input.up = true;
+        }
+        if(wpad->exp.nunchuk.js.pos.y <= 0x40){
+            input.down = true;
+        }
+        if(wpad->exp.nunchuk.js.pos.x <= 0x40){
+            input.left = true;
+        }
+        if(wpad->exp.nunchuk.js.pos.x >= 0xB0){
+            input.right = true;
+        }
+    } else {
+        // No attachment
+        if(wpad->btns_h & WPAD_BUTTON_UP){
+            if(wpad->ir.valid){
+                input.up = true;
+            } else {
+                input.left = true;
+            }
+        }
+        if(wpad->btns_h & WPAD_BUTTON_DOWN){
+            if(wpad->ir.valid){
+                input.down = true;
+            } else {
+                input.right = true;
+            }
+        }
+        if(wpad->btns_h & WPAD_BUTTON_LEFT){
+            if(wpad->ir.valid){
+                input.left = true;
+            } else {
+                input.down = true;
+            }
+        }
+        if(wpad->btns_h & WPAD_BUTTON_RIGHT){
+            if(wpad->ir.valid){
+                input.right = true;
+            } else {
+                input.up = true;
+            }
+        }
+    } 
+    
+    // Gamecube controller
+    if(gcheld & PAD_BUTTON_UP){
         input.up = true;
     }
-    
-    // Gamecube Pad of course this assumes in the first position
-    u16 gcheld = PAD_ButtonsHeld(0);
-    if(gcheld & PAD_BUTTON_A){
-        input.button1 = true;
-    } else if(gcheld & PAD_BUTTON_B){
-        input.button2 = true;
-    } else if(gcheld & PAD_BUTTON_Y){
-        input.button4 = true;
-    } else if(gcheld & PAD_BUTTON_X){
-        input.button3 = true;
-    } else if(gcheld & PAD_BUTTON_UP){
-        input.up = true;
-    } else if(gcheld & PAD_BUTTON_DOWN){
+    if(gcheld & PAD_BUTTON_DOWN){
         input.down = true;
-    } else if(gcheld & PAD_BUTTON_LEFT){
+    }
+    if(gcheld & PAD_BUTTON_LEFT){
         input.left = true;
-    } else if(gcheld & PAD_BUTTON_RIGHT){
+    }
+    if(gcheld & PAD_BUTTON_RIGHT){
         input.right = true;
-    } else if(gcheld & PAD_BUTTON_START){
+    }
+    
+    // Check all input buttons
+    if (wpad->btns_h & WPAD_CLASSIC_BUTTON_X || wpad->btns_h & WPAD_BUTTON_1 || gcheld & PAD_BUTTON_X){
+        input.button1 = true;
+    }
+    if (wpad->btns_h & WPAD_CLASSIC_BUTTON_Y || wpad->btns_h & WPAD_BUTTON_2 || gcheld & PAD_BUTTON_Y){
+        input.button2 = true;
+    }
+    if (wpad->btns_h & WPAD_CLASSIC_BUTTON_A || wpad->btns_h & WPAD_BUTTON_A || gcheld & PAD_BUTTON_A){
+        input.button3 = true;
+    }
+    if (wpad->btns_h & WPAD_CLASSIC_BUTTON_B || wpad->btns_h & WPAD_BUTTON_B || gcheld & PAD_BUTTON_B){
+        input.button4 = true;
+    }
+    if (wpad->btns_h & WPAD_CLASSIC_BUTTON_PLUS || wpad->btns_h & WPAD_BUTTON_PLUS || gcheld & PAD_BUTTON_START){
         // enter
-    } else if(gcheld & PAD_BUTTON_MENU){
+    }
+    if (wpad->btns_h & WPAD_CLASSIC_BUTTON_HOME || wpad->btns_h & WPAD_BUTTON_HOME || gcheld & PAD_BUTTON_MENU){
         // esc
+    }
+    if (wpad->btns_h & WPAD_CLASSIC_BUTTON_MINUS || wpad->btns_h & WPAD_BUTTON_MINUS){
+        // ?
     }  
 
     return input;
