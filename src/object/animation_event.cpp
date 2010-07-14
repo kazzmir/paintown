79dec01b774ee1a006509fd6fbb5b7f30a1ae2ad
Jon Rafkind <jon@rafkind.com>
2010-07-14 05:06:39 +0000

    move animation_event classes to one file

--- src/object/animation_event.cpp
+++ src/object/animation_event.cpp
@@ -1,5 +1,14 @@
 #include "animation.h"
 #include "animation_event.h"
+#include "projectile.h"
+#include "util/token.h"
+#include "util/token_exception.h"
+#include "util/tokenreader.h"
+#include "util/funcs.h"
+#include "util/file-system.h"
+#include <string>
+
+using namespace std;
 
 AnimationEvent::AnimationEvent( ){
 }
@@ -24,3 +33,225 @@ void AnimationEventAttack::Interact( Animation * animation ){
 	animation->setDamage( damage );
 }
 */
+
+AnimationEventBBox::AnimationEventBBox( int _x1, int _y1, int _x2, int _y2 ):
+x1( _x1 ),
+y1( _y1 ),
+x2( _x2 ),
+y2( _y2 ){
+}
+
+
+void AnimationEventBBox::Interact( Animation * animation ){
+	animation->setBBox( x1, y1, x2, y2 );
+}
+
+AnimationEventCoords::AnimationEventCoords( int _x, int _y, int _z ):
+AnimationEvent(),
+x( _x ),
+y( _y ),
+z( _z ){
+}
+
+void AnimationEventCoords::Interact( Animation * animation ){
+	if ( x != -1 )
+		animation->setX( x );
+	if ( y != -1 )
+		animation->setY( y );
+	if ( z != -1 )
+		animation->setZ( z );
+}
+
+AnimationEventDelay::AnimationEventDelay( int _delay ):
+AnimationEvent(),
+delay( _delay ){
+}
+
+void AnimationEventDelay::Interact( Animation * animation ){
+
+	animation->setDelay( delay );
+
+}
+
+AnimationEventFace::AnimationEventFace( int direction ):
+direction( direction ){
+}
+
+void AnimationEventFace::Interact( Animation * animation ){
+	animation->setFacing( direction );
+}
+
+AnimationEventFrame::AnimationEventFrame( const string & _path ){
+    path = _path;
+}
+
+void AnimationEventFrame::Interact( Animation * animation ){
+    animation->setFrame( path );
+    animation->Delay();
+}
+
+AnimationEventFrame::~AnimationEventFrame(){
+}
+
+AnimationEventJump::AnimationEventJump( double vx, double vy, double vz ):
+vx( vx ),
+vy( vy ),
+vz( vz ){
+}
+
+void AnimationEventJump::Interact( Animation * animation ){
+	animation->jump( vx, vy, vz );
+}
+
+AnimationEventMove::AnimationEventMove( int _x, int _y, int _z ):
+x( _x ),
+y( _y ),
+z( _z ){
+}
+
+void AnimationEventMove::Interact( Animation * animation ){
+	animation->moveX( x );
+	animation->moveY( y );
+	animation->moveZ( z );
+}
+
+AnimationEventNOP::AnimationEventNOP():
+AnimationEvent(){
+}
+
+void AnimationEventNOP::Interact( Animation * animation ){
+	animation->Delay();
+}
+
+AnimationEventOffset::AnimationEventOffset( int _x, int _y ):
+AnimationEvent(),
+x( _x ),
+y( _y ){
+}
+
+void AnimationEventOffset::Interact( Animation * animation ){
+	animation->setOffsetX( x );
+	animation->setOffsetY( y );
+}
+
+AnimationEventProjectile::AnimationEventProjectile( Token * token ) throw( LoadException ):
+projectile( NULL ),
+x( 0 ),
+y( 0 ),
+dx( 0 ),
+dy( 0 ),
+life( 0 ){
+	if ( *token != "projectile" ){
+		throw LoadException(__FILE__, __LINE__, "Token starts with " + token->getName() + " instead of 'projectile'." );
+	}
+
+	while ( token->hasTokens() ){
+		Token * current;
+		*token >> current;
+		if ( *current == "at" ){
+			int x, y;
+			*current >> x >> y;
+			setX( x );
+			setY( y );
+		} else if ( *current == "path" ){
+			string path;
+			*current >> path;
+                        Filesystem::AbsolutePath full = Filesystem::find(Filesystem::RelativePath(path));
+			TokenReader reader(full.path());
+			try{
+				projectile = new Projectile(reader.readToken());
+			} catch ( const TokenException & ex ){
+				// cerr<< "Could not read " << full.path() <<" : " << ex.getReason() << endl;
+				// delete head;
+				throw LoadException(__FILE__, __LINE__, ex, "Could not open projectile file: " + full.path());
+			}
+		} else if ( *current == "life" ){
+			int life;
+			*current >> life;
+			setLife( life );
+		} else if ( *current == "speed" ){
+			double dx, dy;
+			*current >> dx >> dy;
+			setDX( dx );
+			setDY( dy );
+		}
+	}
+
+	if ( projectile == NULL ){
+		throw LoadException(__FILE__, __LINE__, "No 'path' token given for projectile" );	
+	}
+
+	if ( life == 0 ){
+		throw LoadException(__FILE__, __LINE__, "No 'life' token given for projectile" );
+	}
+}
+
+void AnimationEventProjectile::Interact( Animation * animation ){
+	Projectile * copy = (Projectile *) projectile->copy();
+	copy->setDX( getDX() );
+	copy->setDY( getDY() );
+	copy->setLife( getLife() );
+	animation->createProjectile( getX(), -getY(), copy );
+}
+	
+AnimationEventProjectile::~AnimationEventProjectile(){
+	if ( projectile != NULL ){
+		delete projectile;
+	}
+}
+
+AnimationEventShadow::AnimationEventShadow( int x, int y ):
+AnimationEvent(),
+x( x ),
+y( y ){
+}
+
+void AnimationEventShadow::Interact( Animation * animation ){
+	animation->setShadowX( x );
+	animation->setShadowY( y );
+}
+
+AnimationEventSound::AnimationEventSound( const string & _path ){
+	path = _path;
+}
+
+void AnimationEventSound::Interact( Animation * animation ){
+	animation->playSound( path );
+}
+	
+AnimationEventSound::~AnimationEventSound(){
+}
+
+AnimationEventStatus::AnimationEventStatus( int _stat ):
+AnimationEvent(),
+status( _stat ){
+}
+
+void AnimationEventStatus::Interact( Animation * animation ){
+	animation->setStatus( status );
+}
+
+AnimationEventTicket::AnimationEventTicket():
+AnimationEvent(){
+}
+
+void AnimationEventTicket::Interact( Animation * animation ){
+	animation->nextTicket();
+}
+
+AnimationEventTrail::AnimationEventTrail(const int produce, const int life):
+produce(produce),
+life(life){
+}
+
+void AnimationEventTrail::Interact(Animation * animation){
+    animation->setTrails(produce, life);
+}
+
+AnimationEventZDistance::AnimationEventZDistance( const double d ):
+d( d ){
+}
+
+void AnimationEventZDistance::Interact( Animation * animation ){
+	animation->setMinZDistance( d );
+}
--- src/object/animation_event.h
+++ src/object/animation_event.h
@@ -2,7 +2,10 @@
 #define _paintown_animation_event_h
 
 #include "attack.h"
+#include "util/load_exception.h"
 
+class Token;
+class Projectile;
 class Animation;
 
 class AnimationEvent{
@@ -23,6 +26,221 @@ public:
 
 protected:
         Attack attack;
+};
+
+class AnimationEventBBox: public AnimationEvent {
+public:
+	AnimationEventBBox( int _x1, int _y1, int _x2, int _y2 );
+
+	virtual void Interact( Animation * animation ); 
+
+protected:
+	int x1, y1, x2, y2;
+
+};
+
+class AnimationEventCoords: public AnimationEvent{
+public:
+	AnimationEventCoords( int _x, int _y, int _z );
+
+	virtual void Interact( Animation * animation ); 
+
+protected:
+	int x, y, z;
+
+};
+
+class AnimationEventDelay: public AnimationEvent{
+public:
+	AnimationEventDelay( int _delay );
+
+	virtual void Interact( Animation * animation );
+
+protected:
+	int delay;
+};
+
+class AnimationEventFace: public AnimationEvent{
+public:
+	AnimationEventFace( int direction );
+
+	virtual void Interact( Animation * animation ); 
+
+protected:
+	int direction;
+
+};
+
+class AnimationEventFrame: public AnimationEvent{
+public:
+	AnimationEventFrame( const std::string & _path );
+
+	virtual void Interact( Animation * animation ); 
+
+	virtual ~AnimationEventFrame();
+
+protected:
+        std::string path;
+};
+
+class AnimationEventJump: public AnimationEvent{
+public:
+	AnimationEventJump( double vx, double vy, double vz );
+
+	virtual void Interact( Animation * animation ); 
+
+protected:
+	double vx, vy, vz;
+
+};
+
+class AnimationEventMove: public AnimationEvent{
+public:
+	AnimationEventMove( int _x, int _y, int _z );
+
+	virtual void Interact( Animation * animation ); 
+
+protected:
+	int x, y, z;
+
+};
+
+class AnimationEventNOP: public AnimationEvent{
+public:
+	AnimationEventNOP();
+
+	virtual void Interact( Animation * animation ); 
+
+};
+
+class AnimationEventOffset: public AnimationEvent{
+public:
+	AnimationEventOffset( int _x, int _y );
+
+	virtual void Interact( Animation * animation ); 
+
+protected:
+	int x, y;
+
+};
+
+class AnimationEventProjectile: public AnimationEvent {
+public:
+	AnimationEventProjectile( Token * token ) throw( LoadException );
+
+	virtual void Interact( Animation * animation ); 
+
+	virtual ~AnimationEventProjectile();
+
+protected:
+	inline void setX( int x ){
+		this->x = x;
+	}
+
+	inline void setY( int y ){
+		this->y = y;
+	}
+
+	inline int getX() const {
+		return x;
+	}
+
+	inline int getY() const {
+		return y;
+	}
+
+	inline void setDX( double dx ){
+		this->dx = dx;
+	}
+
+	inline void setDY( double dy ){
+		this->dy = dy;
+	}
+
+	inline double getDX() const {
+		return this->dx;
+	}
+	
+	inline double getDY() const {
+		return this->dy;
+	}
+
+	inline void setLife( int life ){
+		this->life = life;
+	}
+
+	inline int getLife() const {
+		return life;
+	}
+
+	Projectile * projectile;
+	int x, y;
+	double dx, dy;
+	int life;
+};
+
+class AnimationEventShadow: public AnimationEvent {
+public:
+	AnimationEventShadow( int x, int y );
+
+	virtual void Interact( Animation * animation ); 
+
+protected:
+
+	int x, y;
+};
+
+class AnimationEventSound: public AnimationEvent {
+public:
+	AnimationEventSound( const std::string & path );
+
+	virtual void Interact( Animation * animation ); 
+
+	virtual ~AnimationEventSound();
+
+protected:
+
+        std::string path;
+
+};
+
+class AnimationEventStatus: public AnimationEvent{
+public:
+	AnimationEventStatus( int _stat );
+
+	virtual void Interact( Animation * animation ); 
+
+protected:
+	int status;
+
+};
+
+class AnimationEventTicket: public AnimationEvent{
+public:
+	AnimationEventTicket();
+
+	virtual void Interact( Animation * animation ); 
+
+};
+
+class AnimationEventTrail: public AnimationEvent {
+public:
+    AnimationEventTrail(const int produce, const int life);
+    virtual void Interact( Animation * animation ); 
+
+protected:
+	const int produce;
+        const int life;
+};
+
+class AnimationEventZDistance: public AnimationEvent {
+public:
+	AnimationEventZDistance( const double d );
+
+	virtual void Interact( Animation * animation ); 
+
+protected:
+	double d;
 };
 
 #endif
