32a63380e6f451e92dcd55f49e7cd20452e3119b
Jon Rafkind <jon@rafkind.com>
2010-07-25 06:14:58 +0000

    compile mugen test stage

--- src/test/mugen/load-stage.cpp
+++ src/test/mugen/load-stage.cpp
@@ -1,8 +1,13 @@
+#ifdef USE_ALLEGRO
 #include <allegro.h>
+#elif USE_SDL
+#include <SDL/SDL.h>
+#endif
 
+#include "util/bitmap.h"
 #include "util/file-system.h"
-#include "mugen/mugen_stage.h"
-#include "mugen/mugen_exception.h"
+#include "mugen/stage.h"
+#include "mugen/exception.h"
 #include "globals.h"
 #include "input/input-manager.h"
 
@@ -11,21 +16,34 @@
 using namespace std;
 
 int main(int argc, char ** argv){
+#ifdef USE_ALLEGRO
     install_allegro(SYSTEM_NONE, &errno, atexit);
     set_color_depth(16);
     set_color_conversion(COLORCONV_NONE);
+#endif
+#ifdef USE_SDL
+    SDL_Init(SDL_INIT_VIDEO);
+#endif
+    Bitmap::setFakeGraphicsMode(640, 480);
     InputManager input;
 
     // for (int i = 0; i < 3; i++){
         try{
-            MugenStage stage(Filesystem::find("mugen/stages/kfm.def"));
+            const char * file = "mugen/stages/kfm.def";
+            Global::debug(0, "test") << "Loading " << file << endl;
+            MugenStage stage(Filesystem::find(Filesystem::RelativePath(file)));
             stage.load();
             Global::debug(0, "test") << "Success" << endl;
         } catch (const MugenException & e){
             Global::debug(0, "test") << "Exception: " << e.getReason() << endl;
         } catch (const Filesystem::NotFound & e){
-            Global::debug(0, "test") << "Exception: " << e.getReason() << endl;
+            Global::debug(0, "test") << "Exception: " << e.getTrace() << endl;
         }
     // }
+#ifdef USE_SDL
+        SDL_Quit();
+#endif
 }
+#ifdef USE_ALLEGRO
 END_OF_MAIN()
+#endif
