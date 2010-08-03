f4e8de1b72cb37fcbbfa1563a02445df0d0d9c1d
Jon Rafkind <jon@rafkind.com>
2010-08-03 02:05:48 +0000

    move state controller implementation to its own file

--- cmake/FileList.cmake
+++ cmake/FileList.cmake
@@ -195,6 +195,7 @@ src/mugen/sound.cpp
 src/mugen/sprite.cpp
 src/mugen/stage.cpp
 src/mugen/util.cpp
+src/mugen/state-controller.cpp
 src/mugen/option-arcade.cpp
 src/mugen/option-options.cpp
 src/mugen/option-versus.cpp
