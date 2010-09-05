8ffccb0fe5280b4cc5b4ea29862bbfbf77b067d2
Jon Rafkind <jon@rafkind.com>
2010-09-05 20:28:45 +0000

    add definitions file

---
+++ src/mugen/definitions.h
@@ -0,0 +1,24 @@
+#ifndef _paintown_mugen_definition_h
+#define _paintown_mugen_definition_h
+
+/* some shared definitions */
+
+namespace Mugen{
+
+namespace AttackType{
+    enum Attribute{
+        NormalAttack,
+        NormalThrow,
+        NormalProjectile,
+        SpecialAttack,
+        SpecialThrow,
+        SpecialProjectile,
+        HyperAttack,
+        HyperThrow,
+        HyperProjectile
+    };
+}
+
+}
+
+#endif
