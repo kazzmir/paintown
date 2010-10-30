b197888d484525c9704c09d38d11fd39e7e851d8
Jon Rafkind <jon@rafkind.com>
2010-10-30 18:03:11 +0000

    move gme to util/

--- src/test/mugen/load.cpp
+++ src/test/mugen/load.cpp
@@ -38,7 +38,7 @@ static void showMemory(){
 
 static int load(const char * path){
     // showMemory();
-    for (int i = 0; i < 1; i++){
+    for (int i = 0; i < 5; i++){
         try{
             TimeDifference diff;
             diff.startTime();
