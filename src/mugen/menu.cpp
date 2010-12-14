4463bcbcc85fd3e106f6c7db94528462b315967d
Jon Rafkind <jon@rafkind.com>
2010-12-14 08:02:00 +0000

    use util to parse

--- src/mugen/menu.cpp
+++ src/mugen/menu.cpp
@@ -241,7 +241,7 @@ void MugenMenu::loadData(){
     try{
         TimeDifference diff;
         diff.startTime();
-        Ast::AstParse parsed(Mugen::ParseCache::parseDef(ourDefFile.path()));
+        Ast::AstParse parsed(Mugen::Util::parseDef(ourDefFile.path()));
         diff.endTime();
         Global::debug(1) << "Parsed mugen file " + ourDefFile.path() + " in" + diff.printTime("") << endl;
 
