e5ea366ffa8888d7f1d12fcf7942fc435bb302e4
Jon Rafkind <jon@rafkind.com>
2011-01-24 23:17:49 +0000

    add build file

--- tools/mugen-converter/src/main.cpp
+++ tools/mugen-converter/src/main.cpp
@@ -42,6 +42,20 @@ namespace Mugen{
     }
 }
 
+string Util::trim(const std::string & str){
+    string s;
+    size_t startpos = str.find_first_not_of(" \t");
+    size_t endpos = str.find_last_not_of(" \t");
+    // if all spaces or empty return an empty string  
+    if ((string::npos == startpos) ||
+        (string::npos == endpos)){
+        return "";
+    } else {
+        return str.substr(startpos, endpos-startpos+1);
+    }
+    return str;
+}
+
 void destroy(std::list<Ast::Section*> * list){
     for (std::list<Ast::Section*>::iterator it = list->begin(); it != list->end(); it++){
         delete (*it);
