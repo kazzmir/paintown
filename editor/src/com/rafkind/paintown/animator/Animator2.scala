00d119f5267859c4ff24081ba6ee8fdd82f87ed1
Jon Rafkind <jon@rafkind.com>
2011-05-01 18:38:34 +0000

    set up animator frame

--- editor/src/com/rafkind/paintown/animator/Animator2.scala
+++ editor/src/com/rafkind/paintown/animator/Animator2.scala
@@ -1,2 +1,25 @@
+package com.rafkind.paintown.animator
+
+import java.awt.Toolkit
+import javax.swing._
+
+class NewAnimator extends JFrame("Paintown Animator"){
+  def construct(){
+    val screen = Toolkit.getDefaultToolkit().getScreenSize()
+    this.setSize((screen.getWidth() * 0.9).toInt,
+                 (screen.getHeight() * 0.9).toInt);
+  }
+
+  construct()
+}
+
 object Animator2{
+  def main(args: Array[String]):Unit = {
+    val editor = new NewAnimator();
+    SwingUtilities.invokeLater(new Runnable(){
+        def run(){
+          editor.setVisible(true);
+        }
+      });
+  }
 }
