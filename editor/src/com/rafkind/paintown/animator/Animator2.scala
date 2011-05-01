b6be254a1de6f09de88a2816a44f66fdae8cfe62
Jon Rafkind <jon@rafkind.com>
2011-05-01 21:35:12 +0000

    better namespace management

--- editor/src/com/rafkind/paintown/animator/Animator2.scala
+++ editor/src/com/rafkind/paintown/animator/Animator2.scala
@@ -1,28 +1,25 @@
 package com.rafkind.paintown.animator
 
-import java.awt.Event
-import java.awt.event._
-import java.awt.Toolkit
-import javax.swing.event._
-import javax.swing._
+import java.awt
+import javax.swing
 
 import com.rafkind.paintown._
 
-class NewAnimator extends JFrame("Paintown Animator"){
+class NewAnimator extends swing.JFrame("Paintown Animator"){
   def construct(){
-    val screen = Toolkit.getDefaultToolkit().getScreenSize()
+    val screen = awt.Toolkit.getDefaultToolkit().getScreenSize()
     this.setSize((screen.getWidth() * 0.9).toInt,
                  (screen.getHeight() * 0.9).toInt);
 
     Closer.open()
 
-    val menuBar = new JMenuBar();
-    val menuProgram = new JMenu("Program");
-    val levelEditor = new JMenuItem("Run the level editor");
-    val quit = new JMenuItem("Quit");
-    val clearCache = new JMenuItem("Clear image cache");
-    val data = new JMenuItem("Data path");
-    val closeTab = new JMenuItem("Close Tab");
+    val menuBar = new swing.JMenuBar();
+    val menuProgram = new swing.JMenu("Program");
+    val levelEditor = new swing.JMenuItem("Run the level editor");
+    val quit = new swing.JMenuItem("Quit");
+    val clearCache = new swing.JMenuItem("Clear image cache");
+    val data = new swing.JMenuItem("Data path");
+    val closeTab = new swing.JMenuItem("Close Tab");
     menuProgram.add(levelEditor);
     menuProgram.add(data);
     menuProgram.add(clearCache);
@@ -30,65 +27,65 @@ class NewAnimator extends JFrame("Paintown Animator"){
     menuProgram.add(quit);
     menuBar.add(menuProgram);
 
-    val menuProjectile = new JMenu("Projectile");
+    val menuProjectile = new swing.JMenu("Projectile");
     menuBar.add(menuProjectile);
 
-    val newProjectile = new JMenuItem("New Projectile");
+    val newProjectile = new swing.JMenuItem("New Projectile");
     menuProjectile.add(newProjectile);
-    val openProjectile = new JMenuItem("Open Projectile");
+    val openProjectile = new swing.JMenuItem("Open Projectile");
     menuProjectile.add(openProjectile);
-    val saveProjectile = new JMenuItem("Save Projectile");
+    val saveProjectile = new swing.JMenuItem("Save Projectile");
     menuProjectile.add(saveProjectile);
 
-    val menuCharacter = new JMenu("Character");
+    val menuCharacter = new swing.JMenu("Character");
     menuBar.add(menuCharacter);
 
-    val newCharacter = new JMenuItem("New Character");
+    val newCharacter = new swing.JMenuItem("New Character");
     menuCharacter.add(newCharacter);
-    val loadCharacter = new JMenuItem("Open Character");
+    val loadCharacter = new swing.JMenuItem("Open Character");
     menuCharacter.add(loadCharacter);
-    val saveCharacter = new JMenuItem("Save Character");
+    val saveCharacter = new swing.JMenuItem("Save Character");
     menuCharacter.add(saveCharacter);
-    val saveCharacterAs = new JMenuItem("Save Character As");
+    val saveCharacterAs = new swing.JMenuItem("Save Character As");
     menuCharacter.add(saveCharacterAs);
 
-    menuProgram.setMnemonic(KeyEvent.VK_P);
-    data.setMnemonic(KeyEvent.VK_D);
-    closeTab.setMnemonic(KeyEvent.VK_C);
-    closeTab.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, Event.CTRL_MASK));
-    quit.setMnemonic(KeyEvent.VK_Q);
-    quit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, Event.CTRL_MASK));
-    newCharacter.setMnemonic(KeyEvent.VK_N);
-    newCharacter.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, Event.CTRL_MASK));
-    menuCharacter.setMnemonic(KeyEvent.VK_H);
-    saveCharacter.setMnemonic(KeyEvent.VK_S);
-    saveCharacter.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, Event.CTRL_MASK));
-    saveCharacterAs.setMnemonic(KeyEvent.VK_A);
-    saveCharacterAs.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, Event.CTRL_MASK));
-    loadCharacter.setMnemonic(KeyEvent.VK_O);
-    loadCharacter.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, Event.CTRL_MASK));
+    menuProgram.setMnemonic(awt.event.KeyEvent.VK_P);
+    data.setMnemonic(awt.event.KeyEvent.VK_D);
+    closeTab.setMnemonic(awt.event.KeyEvent.VK_C);
+    closeTab.setAccelerator(swing.KeyStroke.getKeyStroke(awt.event.KeyEvent.VK_W, awt.Event.CTRL_MASK));
+    quit.setMnemonic(awt.event.KeyEvent.VK_Q);
+    quit.setAccelerator(swing.KeyStroke.getKeyStroke(awt.event.KeyEvent.VK_Q, awt.Event.CTRL_MASK));
+    newCharacter.setMnemonic(awt.event.KeyEvent.VK_N);
+    newCharacter.setAccelerator(swing.KeyStroke.getKeyStroke(awt.event.KeyEvent.VK_N, awt.Event.CTRL_MASK));
+    menuCharacter.setMnemonic(awt.event.KeyEvent.VK_H);
+    saveCharacter.setMnemonic(awt.event.KeyEvent.VK_S);
+    saveCharacter.setAccelerator(swing.KeyStroke.getKeyStroke(awt.event.KeyEvent.VK_S, awt.Event.CTRL_MASK));
+    saveCharacterAs.setMnemonic(awt.event.KeyEvent.VK_A);
+    saveCharacterAs.setAccelerator(swing.KeyStroke.getKeyStroke(awt.event.KeyEvent.VK_A, awt.Event.CTRL_MASK));
+    loadCharacter.setMnemonic(awt.event.KeyEvent.VK_O);
+    loadCharacter.setAccelerator(swing.KeyStroke.getKeyStroke(awt.event.KeyEvent.VK_O, awt.Event.CTRL_MASK));
 
     this.setJMenuBar(menuBar);
-    this.addWindowListener(new WindowAdapter(){
-      override def windowClosing(e:WindowEvent){
+    this.addWindowListener(new awt.event.WindowAdapter(){
+      override def windowClosing(e:awt.event.WindowEvent){
         Closer.close();
       }
     });
 
-    levelEditor.addActionListener(new AbstractAction(){
-      override def actionPerformed(event:ActionEvent){
+    levelEditor.addActionListener(new swing.AbstractAction(){
+      override def actionPerformed(event:awt.event.ActionEvent){
         com.rafkind.paintown.Editor2.main(new Array[String](0));
       }
     });
 
-    quit.addActionListener(new ActionListener() {
-      override def actionPerformed(event:ActionEvent){
+    quit.addActionListener(new awt.event.ActionListener() {
+      override def actionPerformed(event:awt.event.ActionEvent){
         Closer.close()
       }
     });
 
-    clearCache.addActionListener( new ActionListener(){
-      override def actionPerformed(event:ActionEvent){
+    clearCache.addActionListener(new awt.event.ActionListener(){
+      override def actionPerformed(event:awt.event.ActionEvent){
         MaskedImage.clearCache();
       }
     });
@@ -100,7 +97,7 @@ class NewAnimator extends JFrame("Paintown Animator"){
 object Animator2{
   def main(args: Array[String]):Unit = {
     val editor = new NewAnimator();
-    SwingUtilities.invokeLater(new Runnable(){
+    swing.SwingUtilities.invokeLater(new Runnable(){
         def run(){
           editor.setVisible(true);
         }
