74944d5583864117a72b56b1aa6774a504c4e14f
Jon Rafkind <jon@rafkind.com>
2011-05-15 22:27:13 +0000

    add missing header file

---
+++ src/paintown-engine/game/options.h
@@ -0,0 +1,26 @@
+#ifndef _paintown_game_menu_options
+#define _paintown_game_menu_options
+
+#include "menu/optionfactory.h"
+#include "paintown-engine/level/utils.h"
+
+class MenuOption;
+namespace Menu{
+    class Context;
+}
+
+namespace Paintown{
+
+/* FIXME figure out what to do with this, since network depends on it." */
+Level::LevelInfo doLevelMenu(const std::string dir, const Menu::Context & context);
+
+class OptionFactory: public Menu::OptionFactory {
+public:
+    OptionFactory();
+    virtual MenuOption * getOption(const Token *token) const;
+    virtual ~OptionFactory();
+};
+
+}
+
+#endif
