235b8e06a9f9ed31498768358a17d17e53e6c78c
Jon Rafkind <jon@rafkind.com>
2011-08-15 23:43:41 +0000

    move gme and sdl-mixer to their own sconscript files. work on the audio test a bit

--- src/test/audio/test.cpp
+++ src/test/audio/test.cpp
@@ -2,6 +2,26 @@
 #include <string>
 #include "util/music-player.h"
 
+class Sound{
+public:
+    struct SoundInfo{
+        SoundInfo():
+            frequency(22050),
+            channels(2),
+            format(0){
+            }
+
+        int frequency;
+        int channels;
+
+        /* format is mostly for SDL */
+        int format;
+    };
+    static SoundInfo Info;
+};
+
+Sound::SoundInfo Sound::Info;
+
 using namespace std;
 
 void play(const string & path){
