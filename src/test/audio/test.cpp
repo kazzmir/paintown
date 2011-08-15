397afab6293dbded8478ba61412c21795da19075
Jon Rafkind <jon@rafkind.com>
2011-08-15 23:55:08 +0000

    play audio in the test

--- src/test/audio/test.cpp
+++ src/test/audio/test.cpp
@@ -25,9 +25,35 @@ Sound::SoundInfo Sound::Info;
 using namespace std;
 
 void play(const string & path){
+    Util::Mp3Player player(path);
+    player.play();
+
+    while (true){
+        SDL_Delay(1);
+    }
 }
 
-void initialize(){
+void initialize(int rate){
+    SDL_Init(SDL_INIT_AUDIO);
+    atexit(SDL_Quit);
+
+    int audio_rate = rate;
+    // int audio_rate = 22050;
+    Uint16 audio_format = AUDIO_S16; 
+    // Uint16 audio_format = MIX_DEFAULT_FORMAT; 
+    int audio_channels = 2;
+    int audio_buffers = 4096;
+    // int audio_buffers = 44100;
+    if (Mix_OpenAudio(audio_rate, audio_format, audio_channels, audio_buffers)) {
+        printf("Unable to open audio: %s!\n", Mix_GetError());
+        // exit(1);
+    }
+
+    /* use the frequency enforced by the audio system */
+    Mix_QuerySpec(&audio_rate, &audio_format, &audio_channels);
+    Sound::Info.frequency = audio_rate;
+    Sound::Info.channels = audio_channels;
+    Sound::Info.format = audio_format;
 }
 
 int main(int argc, char ** argv){
@@ -35,6 +61,6 @@ int main(int argc, char ** argv){
         cout << "Give an audio file as an argument" << endl;
         return 0;
     }
-    initialize();
+    initialize(48000);
     play(argv[1]);
 }
