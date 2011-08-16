a9bd9597c24fc31d008e34656f57121a5ca6aea6
juvinious <juvinious@gmail.com>
2011-08-16 17:17:34 +0000

    Begin to add trigger conversion utility.

---
+++ tools/mugen-converter/src/triggers.h
@@ -0,0 +1,15 @@
+#ifndef mugen_converter_triggers
+#define mugen_converter_triggers
+
+#include <string>
+
+namespace Mugen{
+
+/*! Trigger Handling converts triggers into python code */
+namespace TriggerHandler{
+
+const std::string convert(const std::string &);
+
+}
+}
+#endif
