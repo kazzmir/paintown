73156e2d468122980ac0e318340ca9b22febecb7
juvinious <juvinious@gmail.com>
2011-08-30 15:31:32 +0000

    Update java package for android.

--- src/test/android/src/com/paintown/Paintown.java
+++ src/test/android/src/com/paintown/Paintown.java
@@ -1,42 +1,15 @@
 package com.paintown;
 
-import java.io.*;
-import android.content.res.AssetManager;
+import android.app.Activity;
+import android.os.Bundle;
 
-public class Paintown extends Activity {
-
-    static AssetManager assetManager;
-    
-    protected void runCommand(String command){
-        try {
-            // Executes the command.
-            Process process = Runtime.getRuntime().exec(command);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    
-    /** Load assets. */
+public class Paintown extends Activity
+{
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle icicle) {
-        
-        assetManager = getAssets();
-        InputStream in = assetManager.open("paintown.so");
-        OutputStream out = new FileOutputStream("/data/data/paintown/com.paintown");
-        byte[] buffer = new byte[1024];
-        int length;
-        while ((length = in.read(buffer)) > 0){
-            out.write(buf, 0, len);
-        }
-        in.close();
-        out.close();
-        try {
-            runCommand("/system/bin/chmod 744 /data/data/paintown/com.paintown");
-            // Run paintown
-            runCommand("/data/data/paintown/com.paintown");
-        } catch (RuntimeException e){
-        }
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
     }
 }
