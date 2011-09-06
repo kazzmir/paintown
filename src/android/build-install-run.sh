b2b4ab8d39afde9de26bd7756600bcfda061a2a8
Jon Rafkind <jon@rafkind.com>
2011-09-06 18:17:17 +0000

    erase old build script

--- src/android/build-install-run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-adb=/opt/android-sdk/platform-tools/adb
-run='am start -a android.intent.action.MAIN -n org.libsdl.app/org.libsdl.app.SDLActivity'
-
-(cd ../..; android=1 make) && ./install.sh && $adb shell $run
-
-# ant debug && $adb uninstall org.libsdl.app && $adb install bin/Paintown-debug.apk
