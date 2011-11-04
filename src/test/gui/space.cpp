1da0acb1310f0dfa9d7b47f334c14b3a131b9676
Jon Rafkind <jon@rafkind.com>
2011-11-04 06:05:18 +0000

    space points use doubles consistently. simple point tests

--- src/test/gui/space.cpp
+++ src/test/gui/space.cpp
@@ -1,3 +1,30 @@
+#include "util/gui/coordinate.h"
+#include "util/debug.h"
+
+namespace Util{
+
+double min(double a, double b){
+    if (a < b){
+        return a;
+    }
+    return b;
+}
+
+double max(double a, double b){
+    if (a > b){
+        return a;
+    }
+    return b;
+}
+
+
+double clamp(double value, double min, double max){
+    return Util::min(Util::max(value, min), max);
+}
+
+}
+
+/*
 #include "../common/init.h"
 #include "../common/timer.h"
 #include "util/debug.h"
@@ -75,9 +102,7 @@ public:
     }
 };
 
-/* test the space api */
-int main(int argc, char ** argv){
-    Global::setDebug(0);   
+static void test1(int argc, char ** argv){
     if (argc > 2){
         int x = atoi(argv[1]), y = atoi(argv[2]);
         Screen::realInit(x, y);
@@ -107,7 +132,41 @@ int main(int argc, char ** argv){
     } else {
         Global::debug(0) << "Usage: ./" << argv[0] << " screen-width screen-height" << std::endl;
     }
+}
+*/
+
+static int test2(){
+    Gui::Space::Space space1(10, 10, 100, 100);
+    Gui::Space::Space space2(5, 5, 20, 20);
+
+    /* two points exactly in the middle */
+    Gui::Space::Point point1 = space1.fromPhysical(320, 240);
+    Gui::Space::Point point2 = space2.fromPhysical(320, 240);
+    if (point1 != point2){
+        Global::debug(0) << "Points from different spaces are not equal!" << std::endl;
+        return 1;
+    }
+
+    Gui::Space::Point point3 = space2.fromPhysical(1, 1);
+    if (point1 == point3){
+        Global::debug(0) << "Points should not be the same" << std::endl;
+        return 1;
+    }
+
+    /* middle + middle = end */
+    Gui::Space::Point point4 = space2.fromPhysical(640, 480);
+    if (point1 + point2 != point4){
+        Global::debug(0) << "Points should be support addition" << std::endl;
+        return 1;
+    }
+
     return 0;
+}
+
+/* test the space api */
+int main(int argc, char ** argv){
+    Global::setDebug(0);   
+    return test2();
 }
 #ifdef USE_ALLEGRO
 END_OF_MAIN()
