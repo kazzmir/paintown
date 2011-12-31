03f8d2877f38b7d9a5f403ef480516d5caa58660
Jon Rafkind <jon@rafkind.com>
2011-12-31 20:21:42 +0000

    [mugen] add implementation file

---
+++ src/mugen/projectile.cpp
@@ -0,0 +1,18 @@
+#include "projectile.h"
+
+namespace Mugen{
+
+Projectile::Projectile(double x, double y, int id, int animation, int hitAnimation, int dieAnimation,
+               int cancelAnimation, double scaleX, double scaleY, bool autoRemove, int removeTime, 
+               double velocityX, double velocityY, double removeVelocityX, double removeVelocityY,
+               double accelerateX, double accelerateY, double velocityXMultipler, 
+               double velocityYMultipler, int hits, int missTime, int priority, int spritePriority,
+               int edge, int stageDistance, int lowBound, int highBound, int shadowRed,
+               int shadowGreen, int shadowBlue, int superMoveTime, int pauseMoveTime,
+               int afterImageTime, int afterImageLength){
+}
+
+Projectile::~Projectile(){
+}
+
+}
