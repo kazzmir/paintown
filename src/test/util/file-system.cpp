13ef04cf21e21dfcdd5f193d102374dbdb651ac6
Jon Rafkind <jon@rafkind.com>
2012-01-08 06:57:09 +0000

    start new file interface

--- src/test/util/file-system.cpp
+++ src/test/util/file-system.cpp
@@ -34,17 +34,11 @@ void testZip(){
     try{
         open("/", O_RDONLY, 0);
         Storage::instance().addOverlay(Filesystem::AbsolutePath("src/test/util/test.zip"), Filesystem::AbsolutePath("test"));
-        std::ifstream x;
-        x.open("test/x.txt");
-        if (x.good()){
-            char line[1024];
-            x.getline(line, sizeof(line));
-            line[x.gcount()] = '\0';
-            std::cout << "Read '" << std::string(line) << std::endl;
-        } else {
-            std::cout << "Could not read test/x.txt!" << std::endl;
-        }
-        x.close();
+        Util::ReferenceCount<Storage::File> file = Storage::instance().open(Filesystem::AbsolutePath("test/SConstruct"));
+        char line[1024];
+        int read = file->readLine(line, sizeof(line) - 1);
+        line[read] = '\0';
+        std::cout << "Read '" << std::string(line) << "'" << std::endl;
     } catch (const Storage::Exception & fail){
         Global::debug(0) << "Zip file error: " << fail.getTrace() << std::endl;
         exit(1);
