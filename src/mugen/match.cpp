2a25dd77824b742eeed4a6ef066d0b9783b4840b
juvinious <juvinious@gmail.com>
2012-02-15 21:02:16 +0000

    [mugen] Add Round/Match management classes to facilitate simplifying the Character HUD.

---
+++ src/mugen/match.cpp
@@ -0,0 +1,45 @@
+#include "match.h"
+
+#include "util/file-system.h"
+
+using namespace Mugen;
+using namespace Match;
+
+int Round::framesPerCount = 0;
+    
+Round::Round(int number):
+roundNumber(number),
+time(0),
+ticker(0),
+winner(NotAwarded),
+state(NotStarted){
+}
+
+Round::~Round(){
+}
+    
+void Round::act(){
+}
+
+void Round::setTime(){
+}
+
+int Round::getTime() const{
+    return time;
+}
+
+void Round::setWinner(const Side player){
+}
+
+const Side & Round::getWinner(){
+    return winner;
+}
+    
+Manager::Manager(const Filesystem::AbsolutePath &){
+}
+
+Manager::~Manager(){
+}
+
+void Manager::act(){
+}
---
+++ src/mugen/match.h
@@ -0,0 +1,75 @@
+#ifndef mugen_match
+#define mugen_match
+
+#include <vector>
+
+#include "util.h"
+
+namespace Mugen{
+    
+/*! Namespace Match for now to avoid conflicts within the HUD */
+namespace Match{
+    
+enum Side{
+    NotAwarded,
+    Player1,
+    Player2,
+    Draw,
+};
+    
+/*! Round data */
+class Round{
+public:
+    Round(int number);
+    virtual ~Round();
+    
+    void act();
+    
+    void setTime();
+    int getTime() const;
+    void setWinner(const Side player);
+    const Side & getWinner();
+    
+    inline static void setFramesPerCount(int count){
+        Round::framesPerCount = count;
+    }
+    
+    enum State{
+        NotStarted,
+        Started,
+        Ended,
+    };
+    
+protected:
+    static int framesPerCount;
+    
+    const int roundNumber;
+    int time;
+    int ticker;
+    Side winner;
+    State state;
+};
+
+/*! Mugen Match Information and Data, houses information regarding the rounds and match */
+class Manager{
+public:
+    Manager(const Filesystem::AbsolutePath &);
+    virtual ~Manager();
+    
+    void act();
+    
+protected:
+    /*! Round information
+     */
+    // Rounds to win
+    int maxWins;
+    // Max Draw Games
+    int maxDrawGames;
+    // Current Round
+    unsigned int currentRound;
+    std::vector<Round> rounds;
+};
+
+}
+}
+#endif
