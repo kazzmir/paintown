38810df692af74eb49312922f51598d994852f70
Jon Rafkind <jon@rafkind.com>
2012-03-19 22:49:12 +0000

    [mugen] dump all the data into one packet

--- src/mugen/network-behavior.cpp
+++ src/mugen/network-behavior.cpp
@@ -1,4 +1,5 @@
 #include "network-behavior.h"
+#include "util/timedifference.h"
 
 using std::vector;
 using std::string;
@@ -15,14 +16,44 @@ socket(socket){
  *   the command string
  */
 static void sendCommands(const vector<string> & commands, Network::Socket socket){
+    /*
+    TimeDifference timer;
+    timer.startTime();
+    */
 #ifdef HAVE_NETWORKING
+    int total = sizeof(uint16_t);
+    for (vector<string>::const_iterator it = commands.begin(); it != commands.end(); it++){
+        const string & command = *it;
+        total += sizeof(uint16_t);
+        total += command.size() + 1;
+    }
+
+    /* Just send one packet */
+    char * buffer = new char[total];
+    char * position = buffer;
+    position = Network::dump16(position, commands.size());
+    for (vector<string>::const_iterator it = commands.begin(); it != commands.end(); it++){
+        const string & command = *it;
+        position = Network::dump16(position, command.size());
+        position = Network::dumpStr(position, command);
+    }
+
+    Network::sendBytes(socket, (uint8_t*) buffer, total);
+
+    /*
     Network::send16(socket, commands.size());
     for (vector<string>::const_iterator it = commands.begin(); it != commands.end(); it++){
         const string & command = *it;
         Network::send16(socket, command.size());
         Network::sendStr(socket, command);
     }
+    */
+    delete[] buffer;
 #endif
+    /*
+    timer.endTime();
+    Global::debug(0) << timer.printTime("send") << std::endl;
+    */
 }
 
 void NetworkLocalBehavior::start(const Stage & stage, Character * owner, const std::vector<Command*> & commands, bool reversed){
@@ -52,6 +83,10 @@ socket(socket){
 
 static vector<string> readCommands(Network::Socket socket){
     vector<string> out;
+    /*
+    TimeDifference timer;
+    timer.startTime();
+    */
 #ifdef HAVE_NETWORKING
     int16_t commands = Network::read16(socket);
     for (int i = 0; i < commands; i++){
@@ -59,6 +94,10 @@ static vector<string> readCommands(Network::Socket socket){
         out.push_back(Network::readStr(socket, length));
     }
 #endif
+    /*
+    timer.endTime();
+    Global::debug(0) << timer.printTime("read") << std::endl;
+    */
     return out;
 }
 
