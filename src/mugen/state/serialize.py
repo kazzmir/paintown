198fe19c9494369460605a791d0af22b0dd92902
Jon Rafkind <jon@rafkind.com>
2012-09-13 05:46:32 +0000

    [mugen] add directory for state

---
+++ src/mugen/state/serialize.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+# This script reads a specification of a datastructure with fields in it and writes
+# out a class that contains those fields and a way to serialize/deserialize them
+# to a stream. This is similar to google's protobuf but using a much simpler
+# implementation.
+
+# TODO: grammar of specification
