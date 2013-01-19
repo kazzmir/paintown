2d88abcc385cedf889d787c5546461f1cf4aee04
Jon Rafkind <jon@rafkind.com>
2013-01-19 22:40:27 +0000

    [mugen] serialize state persistent values

--- src/mugen/world.cpp
+++ src/mugen/world.cpp
@@ -58,11 +58,29 @@ const std::map<CharacterId, AllCharacterData> & World::getCharacterData() const 
     return this->characterData;
 }
 
+static Token * serialize(const map<int, map<unsigned int, int> > & statePersistent){
+    Token * token = new Token();
+
+    *token << "states";
+    for (map<int, map<unsigned int, int> >::const_iterator it = statePersistent.begin(); it != statePersistent.end(); it++){
+        const map<unsigned int, int> & controllers = it->second;
+        Token * out = token->newToken();
+        *out << it->first;
+        for (map<unsigned int, int>::const_iterator it = controllers.begin(); it != controllers.end(); it++){
+            Token * what = out->newToken();
+            *what << it->first << it->second;
+        }
+    }
+
+    return token;
+}
+
 static Token * serialize(const AllCharacterData & data){
     Token * token = new Token();
     *token << "data";
     *token << serialize(data.character);
     *token << serialize(data.animation);
+    *token << serialize(data.statePersistent);
     return token;
 }
 
