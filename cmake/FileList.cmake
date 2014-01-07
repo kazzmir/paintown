fe85dc6a489612392d57b79ac78fa412ca7d9482
juvinious <juvinious@gmail.com>
2014-01-07 19:29:31 +0000

    [platformer] Import module call method and pass in world object.

--- cmake/FileList.cmake
+++ cmake/FileList.cmake
@@ -23,20 +23,6 @@ src/util/graphics/allegro/loadpng/loadpng.c
 src/util/graphics/allegro/loadpng/regpng.c
 src/util/graphics/allegro/loadpng/savepng.c)
 
-set(PLATFORMER_SRC
-src/platformer/argument.cpp
-src/platformer/game/animation.cpp
-src/platformer/game/background.cpp
-src/platformer/game/camera.cpp
-src/platformer/game/collision-map.cpp
-src/platformer/game/game.cpp
-src/platformer/game/options.cpp
-src/platformer/game/tile.cpp
-src/platformer/game/world.cpp
-src/platformer/object/object.cpp
-src/platformer/script/script.cpp
-)
-
 set(MISSILE_DEFEND_SRC
 src/missile-defend/argument.cpp
 src/missile-defend/game.cpp
