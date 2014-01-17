02eb6370c8ba3a74a063703ea498b2938b78bfb3
juvinious <juvinious@gmail.com>
2014-01-17 20:33:05 +0000

    [editor] Add platformer to run.sh script.

--- editor/run.sh
+++ editor/run.sh
@@ -5,6 +5,11 @@ case "$1" in
          shift
          java -classpath editor.jar com.rafkind.paintown.animator.Animator2 $*
          ;;
+ 
+ platformer)
+         shift
+         java -classpath editor.jar com.rafkind.platformer.MapEditor $*
+         ;;
  "")
          echo "To run the character animator: ./run.sh animator"
          java -jar editor.jar -client
