0a1ad3674fb2c5aa5cb42fd46f218a47b07bdfdf
juvinious <juvinious@gmail.com>
2018-04-07 11:43:36 -0400

    Updated cmake build to account for r-tech1 and build paintown.

--- cmake/FileList.cmake
+++ cmake/FileList.cmake
@@ -1,20 +1,8 @@
 set(BASE_SRC 
 src/globals.cpp
-src/main-menu.cpp)
+src/main-menu.cpp
+src/xmain.cpp)
 
 set(FACTORY_SRC
 src/factory/font_render.cpp
 src/factory/collector.cpp)
-
-set(OPENBOR_SRC
-src/openbor/pack-reader.cpp
-src/openbor/animation.cpp
-src/openbor/display-character.cpp
-src/openbor/mod.cpp
-src/openbor/util.cpp
-src/openbor/data.cpp)
-
-#set(LOADPNG_SRC
-#src/util/graphics/allegro/loadpng/loadpng.c
-#src/util/graphics/allegro/loadpng/regpng.c
-#src/util/graphics/allegro/loadpng/savepng.c)
---
+++ cmake/FindRTECH1.cmake
@@ -0,0 +1,52 @@
+# - Find R-Tech1
+# Find the native R-Tech1 includes and library
+#
+#  RTECH1_INCLUDE_DIR - where to find r-tech1 includes
+#  RTECH1_LIBRARIES   - List of libraries when using r-tech1
+#  RTECH1_FOUND       - True if r-tech1 found.
+
+
+IF (RTECH1_INCLUDE_DIR)
+  # Already in cache, be silent
+  SET(RTECH1_FIND_QUIETLY TRUE)
+ENDIF (RTECH1_INCLUDE_DIR)
+
+FIND_PATH(RTECH1_INCLUDE_DIR init.h
+  /usr/local/include/r-tech1
+  /usr/include/r-tech1
+  $ENV{MINGDIR}/include/r-tech1
+)
+
+if(UNIX AND NOT CYGWIN)
+	exec_program(pkg-config ARGS "r-tech1 --libs" OUTPUT_VARIABLE RTECH1_LIBRARY)
+	# Check if any of these checks have found if so then cause failure)
+	STRING(REGEX MATCH "not found" LIB_CHECK ${RTECH1_LIBRARY})
+	IF (${LIB_CHECK})
+	    set(RTECH1_LIBRARY "")
+	ENDIF (${LIB_CHECK})
+endif(UNIX AND NOT CYGWIN)
+
+IF (RTECH1_INCLUDE_DIR AND RTECH1_LIBRARY)
+   SET(RTECH1_FOUND TRUE)
+   SET( RTECH1_LIBRARIES ${RTECH1_LIBRARY} )
+   add_definitions(-DHAVE_CXX11 -DDUMB_DECLARE_DEPRECATED)
+ELSE (RTECH1_INCLUDE_DIR AND RTECH1_LIBRARY)
+   SET(RTECH1_FOUND FALSE)
+   SET( RTECH1_LIBRARIES )
+ENDIF (RTECH1_INCLUDE_DIR AND RTECH1_LIBRARY)
+
+IF (RTECH1_FOUND)
+   IF (NOT RTECH1_FIND_QUIETLY)
+      MESSAGE(STATUS "Found r-tech1: ${RTECH1_LIBRARY}")
+   ENDIF (NOT RTECH1_FIND_QUIETLY)
+ELSE (RTECH1_FOUND)
+   IF (RTECH1_FIND_REQUIRED)
+      MESSAGE(STATUS "Looked for r-tech1 libraries named ${RTECH1_NAMES}.")
+      MESSAGE(FATAL_ERROR "Could NOT find r-tech1 library")
+   ENDIF (RTECH1_FIND_REQUIRED)
+ENDIF (RTECH1_FOUND)
+
+MARK_AS_ADVANCED(
+  RTECH1_LIBRARY
+  RTECH1_INCLUDE_DIR
+  )
