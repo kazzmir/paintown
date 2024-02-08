8bfd38dd6315ef95ca2d9bde325baad876610dff
Humberto Dias <hldtux@gmail.com>
2024-02-08 18:00:00 -0300

    editor build using gradle
    gradle 6 + java 8

--- editor/src/com/rafkind/paintown/animator/Detacher.java
+++
@@ -1,9 +0,0 @@
-package com.rafkind.paintown.animator;
-
-import javax.swing.JComponent;
-import javax.swing.JFrame;
-
-interface Detacher {
-    public JFrame detach(JComponent object, String name);
-    public void attach(JComponent object, String name);
-}
--- editor/src/com/rafkind/paintown/level/EditorException.java
+++
@@ -1,7 +0,0 @@
-package com.rafkind.paintown.level;
-
-public class EditorException extends Exception {
-	public EditorException(String s){
-		super(s);
-	}
-}
