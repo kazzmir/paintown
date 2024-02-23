470a0bcd996330a41e8ce03c3d7a4d1b47c8a9ef
Humberto Dias <hldtux@gmail.com>
2024-02-23 19:51:31 -0300

    initial implementation of fs/system

--- src/r-tech1/psp/file-system.cpp
+++ src/r-tech1/psp/file-system.cpp
@@ -6,9 +6,55 @@
 #include "r-tech1/system.h"
 #include "r-tech1/utf.h"
 
-// TODO
-using namespace std;
+#include <pspiofilemgr.h>
+#include <string>
+#include <vector>
+#include <string>
+#include <cstring> // Include the header file for strcmp
+#include <algorithm> // Include the header file for std::transform
+#include <cctype> // Include the header file for ::tolower
 
+using namespace std;
 vector<Filesystem::AbsolutePath> Filesystem::getFiles(const AbsolutePath & dataPath, const string & find, bool caseInsensitive){
+    std::vector<AbsolutePath> files;
+
+    // Open the directory
+    SceUID dir = sceIoDopen(dataPath.path().c_str());
+    if (dir < 0) {
+        // Error opening directory
+        return files;
+    }
+
+    // Read directory entries
+    SceIoDirent dirent;
+    while (sceIoDread(dir, &dirent) > 0) {
+        // Skip special entries
+        if (std::strcmp(dirent.d_name, ".") == 0 || std::strcmp(dirent.d_name, "..") == 0) {
+            continue;
+        }
+
+        // Check if the filename contains the search string
+        std::string filename = dirent.d_name;
+        if (caseInsensitive) {
+            // Convert both filename and search string to lowercase for case-insensitive comparison
+            std::transform(filename.begin(), filename.end(), filename.begin(), ::tolower);
+            std::string findLower = find;
+            std::transform(findLower.begin(), findLower.end(), findLower.begin(), ::tolower);
+            if (filename.find(findLower) != std::string::npos) {
+                AbsolutePath ap = Filesystem::AbsolutePath(dataPath.path() + "/" + filename);
+                files.push_back(ap);
+            }
+        } else {
+            if (filename.find(find) != std::string::npos) {
+                AbsolutePath ap = Filesystem::AbsolutePath(dataPath.path() + "/" + filename);
+                files.push_back(ap);
+            }
+        }
+    }
+
+    // Close the directory
+    sceIoDclose(dir);
+
+    return files;
 }
 #endif
--- src/r-tech1/psp/system.cpp
+++ src/r-tech1/psp/system.cpp
@@ -5,73 +5,94 @@
 #include <sys/time.h>
 #include "r-tech1/system.h"
 
-/* psp doesn't have an implementation of access()? if it gets one this function
- * can be removed.
- */
-int access(const char * path, int mode){
-    struct stat information;
-    int ok = stat(path, &information);
-    // printf("stat of '%s' is %d\n", path.c_str(), ok);
-    if (ok == 0){
-        if (mode == R_OK){
-            if (((information.st_mode & S_IRUSR) == S_IRUSR) ||
-                ((information.st_mode & S_IRGRP) == S_IRGRP) ||
-                ((information.st_mode & S_IROTH) == S_IROTH)){
-                return 0;
-            } else {
-            /* handle other modes if they become useful to us */
-                return -1;
-            }
-       } else {
-           return -1;
-       }
-    } else {
-        // perror("stat");
-        return -1;
-    }
-}
+#ifdef USE_SDL
+#include <SDL/SDL.h>
+#endif
+
+#ifdef USE_SDL2
+#include <SDL2/SDL.h>
+#endif
+
+#include <cstring>
+#include <cstdlib>
+/**
+ * ref
+ * https://pspdev.github.io/pspsdk/
+*/
+#include <psptypes.h>
+#include <pspkernel.h>
+#include <pspdisplay.h> // For PSP display functions
+#include <pspctrl.h> // For PSP control functions
+#include <pspdebug.h> // For PSP debug functions
+#include <pspctrl.h>
 
 bool System::isDirectory(const std::string & path){
-    // TODO: FIXME
-    return false;
+    SceIoStat stat;
+    int result = sceIoGetstat(path.c_str(), &stat);
+    if (result < 0) {
+        pspDebugScreenPrintf("Error getting file stat: %d\n", result);
+        return false; // Error getting file stat
+    }
+
+    return FIO_S_ISDIR(stat.st_mode);
 }
 
 void System::makeDirectory(const std::string & path){
-    // TODO: FIXME
-}
-
-void makeAllDirectory(const std::string & path){
-    // TODO: FIXME
+    int result = sceIoMkdir(path.c_str(), 0777); // Create directory with full permissions
+    if (result < 0) {
+        // Handle error if mkdir fails
+        pspDebugScreenPrintf("Error creating directory: %d\n", result);
+    }
 }
 
 bool System::readableFile(const std::string & path){
-    // TODO: FIXME
-    return false;
+    return !isDirectory(path) && readable(path);
 }
 
 bool System::readable(const std::string & path){
-    // TODO: FIXME
-    return false;
+    SceUID fd = sceIoOpen(path.c_str(), PSP_O_RDONLY, 0777);
+    if (fd >= 0) {
+        sceIoClose(fd);
+        return true;
+    }
 }
 
 uint64_t System::getModificationTime(const std::string & path){
-    // TODO: FIXME
-    return 0;
+    SceIoStat stat;
+    int result = sceIoGetstat(path.c_str(), &stat);
+    if (result < 0) {
+        return 0; // Return 0 if there's an error
+    }
+
+    return stat.sce_st_mtime.microsecond;
 }
 
 uint64_t System::currentMilliseconds(){
-    // TODO: FIXME
-    return 0;
+#ifdef USE_SDL
+    return SDL_GetTicks();
+#elif USE_SDL2
+    return SDL_GetTicks();
+#else
+
+    // Get the current system time in 64-bit ticks
+    SceKernelWideTime currentTime;
+    sceKernelGetSystemTimeWide(&currentTime);
+
+    // Convert ticks to milliseconds (1 tick = 1000000 nanoseconds)
+    return (currentTime.low / 1000) + (currentTime.hi * 1000000ull);
+#endif
 }
 
+static void * start_memory = 0;
 unsigned long System::memoryUsage(){
-    // TODO: FIXME
-    return 0;
-}
+    void * here = malloc(0);
+    /* hopefully the heap is growing up */
+    return (char*) here - (char*) start_memory;
+ }
 
 /* call startMemoryUsage once at the very beginning of the program */
 void System::startMemoryUsage(){
-    // TODO: FIXME
+    start_memory = malloc(0);
 }
 
 #endif
