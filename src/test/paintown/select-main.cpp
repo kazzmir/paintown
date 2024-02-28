e8f3971f36d20b78620f477b167a2dc2a59fe856
juvinious <581120+juvinious@users.noreply.github.com>
2024-02-28 18:26:12 -0500

    Restore tests.

--- src/test/paintown/select-main.cpp
+++ src/test/paintown/select-main.cpp
@@ -13,9 +13,10 @@
 #include "r-tech1/input/input.h"
 #include "r-tech1/input/input-manager.h"
 #include "r-tech1/font.h"
-#include "r-tech1/system.h"
 
 #include "factory/collector.h"
+
+#include "r-tech1/libs/filesystem/fs-wrapper.h"
 
 //using namespace Mugen;
 
@@ -132,13 +133,9 @@ Filesystem::AbsolutePath Filesystem::userDirectory(){
 
 // Use other directories
 std::string getDataPath(){
-    std::vector<std::string> locations = { "paintown-data",
-                                           "data-new",
-                                           "data-other",
-                                           "/apps/paintown/data",
-                                         };
+    std::vector<std::string> locations = { "paintown-data", "data-new", "data-other" };
     for (std::vector<std::string>::iterator it = locations.begin(); it != locations.end(); it++){
-        if (System::isDirectory(*it)){
+        if (fs::is_directory(*it)){
             return *it;
         }
     }
@@ -146,11 +143,7 @@ std::string getDataPath(){
 }
 
 int test_main(int argc, char** argv){
-#ifndef DEVKITPRO
     if (argc > 1){
-#else
-    if (true) {
-#endif
         Collector janitor;
         Util::setDataPath(getDataPath());
 
@@ -160,11 +153,7 @@ int test_main(int argc, char** argv){
         Global::init(conditions);
         
         Global::setDebug(3);
-#ifndef DEVKITPRO
         std::string file = argv[1];
-#else
-        std::string file = "/apps/paintown/select.txt";
-#endif
 
         Paintown::Mod::loadDefaultMod();
         
@@ -196,6 +185,7 @@ int test_main(int argc, char** argv){
         } catch (const TokenException & ex){
             Global::debug(0) << "Problem parsing file [" << file << "]. Reason: " << ex.getTrace() << std::endl;
         }
+        
     } else {
         std::cout << "Usage: ./" << argv[0] << " select-screen.txt" << std::endl;
     }
--- src/test/util/file-system.cpp
+++ src/test/util/file-system.cpp
@@ -6,15 +6,6 @@
 #include <vector>
 #include "r-tech1/debug.h"
 #include "r-tech1/file-system.h"
-#include "r-tech1/system.h"
-
-#if defined(WII)
-#include <gccore.h>
-#include <wiiuse/wpad.h>
-#include <fat.h>
-#elif defined(SWITCH)
-#include <switch.h>
-#endif
 
 
 #ifndef WINDOWS
@@ -58,147 +49,10 @@ Filesystem::AbsolutePath getDataPath2(){
 */
 }
 
-// Do stuff for switch testing
-class ConsoleWrapper{
-public:
-    ConsoleWrapper():
-    count(0),
-    done(false){
-#if defined(WII)
-         // Initialise the video system
-        VIDEO_Init();
-
-        // This function initialises the attached controllers
-        WPAD_Init();
-
-        // Obtain the preferred video mode from the system
-        // This will correspond to the settings in the Wii menu
-        rmode = VIDEO_GetPreferredMode(NULL);
-
-        // Allocate memory for the display in the uncached region
-        xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
-
-        // Initialise the console, required for printf
-        console_init(xfb,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
-
-        // Set up the video registers with the chosen mode
-        VIDEO_Configure(rmode);
-
-        // Tell the video hardware where our display memory is
-        VIDEO_SetNextFramebuffer(xfb);
-
-        // Make the display visible
-        VIDEO_SetBlack(true);
-
-        // Flush the video register changes to the hardware
-        VIDEO_Flush();
-        VIDEO_WaitVSync();
-        if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
-        fatInitDefault();
-
-#elif defined(SWITCH)
-        consoleInit(NULL);
-        // Configure our supported input layout: a single player with standard controller styles
-        padConfigureInput(1, HidNpadStyleSet_NpadStandard);
-        padInitializeDefault(&pad);
-#endif
-    }
-    ~ConsoleWrapper(){
-#ifdef SWITCH
-        consoleExit(NULL);
-#endif
-    }
-    bool doLoop(){
-#if defined(WII)
-        // Call WPAD_ScanPads each loop, this reads the latest controller states
-        WPAD_ScanPads();
-
-        // WPAD_ButtonsDown tells us which buttons were pressed in this loop
-        // this is a "one shot" state which will not fire again until the button has been released
-        u32 pressed = WPAD_ButtonsDown(0);
-
-        // We return to the launcher application via exit
-        if ( pressed & WPAD_BUTTON_HOME ) 
-            done = true;
-
-        // Wait for the next frame
-        VIDEO_WaitVSync();
-
-        return !done;
-
-#elif defined(SWITCH)
-        // Updade stuff
-        appletMainLoop();
-        // Scan the gamepad. This should be done once for each frame
-        padUpdate(&pad);
-
-        // padGetButtonsDown returns the set of buttons that have been
-        // newly pressed in this frame compared to the previous one
-        u64 kDown = padGetButtonsDown(&pad);
-
-        if (kDown & HidNpadButton_Plus)
-            done = true; // break in order to return to hbmenu
-
-        // Keep going
-        return !done;
-#else
-        if (count++ >= 1){
-            done = true;
-        }
-        return !done;
-#endif
-    }
-    void update(){
-#ifdef SWITCH
-        consoleUpdate(NULL);
-#endif
-    }
-protected:
-#if defined(WII)
-    void *xfb = NULL;
-    GXRModeObj *rmode = NULL;
-#elif defined(SWITCH)
-    // Initialize the default gamepad (which reads handheld mode inputs as well as the first connected controller)
-    PadState pad;
-#endif
-    int count;
-    bool done;
-};
-
-std::string getArchivesPath(){
-#ifdef DEVKITPRO
-    return "/apps/paintown/";
-#else
-    return "src/test/util/";
-#endif
-}
-
-
-// Use other directories
-std::string getDataPath(){
-    std::vector<std::string> locations = { "paintown-data",
-                                           "data-new",
-                                           "data-other",
-                                           "/apps/paintown/data",
-                                         };
-    for (std::vector<std::string>::iterator it = locations.begin(); it != locations.end(); it++){
-        if (System::isDirectory(*it)){
-            return *it;
-        }
-    }
-    return "";
-}
-
-class ExitException : std::exception {
-public:
-    ExitException(){}
-    ~ExitException(){}
-};
-
 
 void testGetFiles(){
     DebugLog << "Testing get files. Setting data path to data." << std::endl;
-    Filesystem::AbsolutePath dataPath = Filesystem::AbsolutePath(getDataPath());
+    Filesystem::AbsolutePath dataPath = Filesystem::AbsolutePath("data");
     DebugLog << "Setting relative path m*/*.txt" << std::endl;
     Filesystem::RelativePath relativePath = Filesystem::RelativePath("m*/*.txt");
     std::vector<Filesystem::AbsolutePath> paths = Storage::instance().getFiles(dataPath, relativePath, false);
@@ -211,7 +65,7 @@ void testGetFiles(){
 void testZip(){
     try{
         // open("/", O_RDONLY, 0);
-        Storage::instance().addOverlay(Filesystem::AbsolutePath(getArchivesPath() + "test.zip"), Filesystem::AbsolutePath("test"));
+        Storage::instance().addOverlay(Filesystem::AbsolutePath("src/test/util/test.zip"), Filesystem::AbsolutePath("test"));
         Util::ReferenceCount<Storage::File> file = Storage::instance().open(Filesystem::AbsolutePath("test/SConstruct"));
         char line[1024];
         int read = file->readLine(line, sizeof(line) - 1);
@@ -219,7 +73,7 @@ void testZip(){
         std::cout << "Read '" << std::string(line) << "'" << std::endl;
     } catch (const Storage::Exception & fail){
         Global::debug(0) << "Zip file error: " << fail.getTrace() << std::endl;
-        throw ExitException();
+        exit(1);
     }
 }
 
@@ -228,7 +82,7 @@ static void testLastComponent(){
     Global::debug(0) << "Last component of " << foo.path() << " is " << foo.getLastComponent() << std::endl;
     if (foo.getLastComponent() != "c"){
         Global::debug(0) << "Last component failed" << std::endl;
-        throw ExitException();
+        exit(1);
     }
 }
 
@@ -247,44 +101,33 @@ static void testDirectory(){
     directory.addFile(Filesystem::AbsolutePath("b/a"), test2);
     if (directory.lookup(Filesystem::AbsolutePath("a/b")) != test1){
         Global::debug(0) << "[1] Directory test failed" << std::endl;
-        throw ExitException();
+        exit(1);
     }
     if (directory.lookup(Filesystem::AbsolutePath("a/../a/./b")) != test1){
         Global::debug(0) << "[2] Directory test failed" << std::endl;
-        throw ExitException();
+        exit(1);
     }
     if (directory.lookup(Filesystem::AbsolutePath("b/a")) != test2){
         Global::debug(0) << "[3] Directory test failed" << std::endl;
-        throw ExitException();
+        exit(1);
     }
 }
 
 static void test7z(){
-    Storage::instance().addOverlay(Filesystem::AbsolutePath(getArchivesPath() + "test.7z"), Filesystem::AbsolutePath("test"));
+    Storage::instance().addOverlay(Filesystem::AbsolutePath("src/test/util/test.7z"), Filesystem::AbsolutePath("test"));
 }
-#ifndef CROSS_BUILD
+#ifndef WINDOWS
 int main(){
 #else
 #include <SDL2/SDL.h>
 int main(int argv, char *args[]){
 #endif
     Global::setDebug(2);
-    ConsoleWrapper cw;
-
-    try {
-        testGetFiles();
-        testZip();
-        testLastComponent();
-        testDirectory();
-        test7z();
-    } catch (const ExitException & ex){
-#ifndef DEVKITPRO
-        exit(0);
-#endif
-    }
-    while(cw.doLoop()){
-        cw.update();
-    }
+    testGetFiles();
+    testZip();
+    testLastComponent();
+    testDirectory();
+    test7z();
 
     return 0;
 }
