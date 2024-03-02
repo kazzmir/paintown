6ffa116ac6b6fdd566ee258dfbf6776794008bfb
Humberto Dias <hldtux@gmail.com>
2024-03-02 11:45:59 -0300

    [psp] add yaml-cpp

--- misc/psp-environment.sh
+++ misc/psp-environment.sh
@@ -2,5 +2,5 @@
 
 echo "
 Isn't necessary to install any dependency.
-Because docker image pspdev/pspdev already contains: SDL2,SDL2_image,SDL2_mixer,SDL2_image
+Because docker image pspdev/pspdev already contains: SDL2,SDL2_image,SDL2_mixer,SDL2_image,SDL2_gfx,yaml-cpp
 "
