de85da19ceb6f86587ae8b112fc66e3214be298d
Jon Rafkind <jon@rafkind.com>
2025-01-15 16:53:12 -0800

    start to load paintown player

--- game/main.go
+++ game/main.go
@@ -3,8 +3,12 @@ package main
 import (
     "log"
     "os"
+    "fmt"
+    "io"
     "image"
     "image/png"
+    "bufio"
+    "path/filepath"
 
     "github.com/kazzmir/paintown/game/lib/coroutine"
 
@@ -40,6 +44,160 @@ func loadPng(path string) (image.Image, error) {
     }
 
     return img, nil
+}
+
+type PaintownCharacter struct {
+    Definition CharacterDefinition
+}
+
+type CharacterDefinition struct {
+    Name string
+}
+
+type SExpr struct {
+    Name string
+    // an sexpr with no children is just a value
+    Children []*SExpr
+}
+
+func (sexpr *SExpr) GetChild(name string) *SExpr {
+    for _, child := range sexpr.Children {
+        if child.Name == name {
+            return child
+        }
+    }
+
+    return nil
+}
+
+func (sexpr *SExpr) IsValue() bool {
+    return len(sexpr.Children) == 0
+}
+
+func (sexpr *SExpr) GetValue(index int) string {
+    if index < len(sexpr.Children) {
+        child := sexpr.Children[index]
+        if child.IsValue() {
+            return child.Name
+        }
+
+        return ""
+    } else {
+        return ""
+    }
+}
+
+func tokenize(reader io.Reader) []string {
+    byteReader, ok := reader.(io.ByteReader)
+    if !ok {
+        byteReader = bufio.NewReader(reader)
+    }
+
+    var tokens []string
+
+    whitespace := func (char byte) bool {
+        return char == ' ' || char == '\t' || char == '\n' || char == '\r'
+    }
+
+    for {
+        char, err := byteReader.ReadByte()
+        if err != nil {
+            break
+        }
+
+        switch {
+            case char == '(':
+                tokens = append(tokens, "(")
+            case char == ')':
+                tokens = append(tokens, ")")
+            case whitespace(char):
+            default:
+                token := ""
+                for char != '(' && char != ')' && !whitespace(char) {
+                    token += string(char)
+                    char, err = byteReader.ReadByte()
+                    if err != nil {
+                        break
+                    }
+                }
+
+                tokens = append(tokens, token)
+
+                if char == '(' {
+                    tokens = append(tokens, "(")
+                } else if char == ')' {
+                    tokens = append(tokens, ")")
+                }
+        }
+    }
+
+    return tokens
+}
+
+func parseSExpr(reader io.Reader) (*SExpr, error) {
+    tokens := tokenize(reader)
+
+    var root *SExpr
+    var current *SExpr
+    var parents []*SExpr
+
+    _ = current
+
+    for _, token := range tokens {
+        if token == "(" {
+            parents = append(parents, current)
+        }
+    }
+
+    if len(parents) != 0 {
+        return nil, fmt.Errorf("Unbalanced open parentheses")
+    }
+
+    return root, nil
+}
+
+func readSExpression(path string) (*SExpr, error) {
+    file, err := os.Open(path)
+    if err != nil {
+        return nil, err
+    }
+    defer file.Close()
+
+    parsed, err := parseSExpr(file)
+    if err != nil {
+        return nil, fmt.Errorf("Unable to parse %v: %v", path, err)
+    }
+
+    return parsed, nil
+}
+
+// a definition file is a parentheses delimited set of values
+func loadDefinition(path string) (CharacterDefinition, error) {
+    raw, err := readSExpression(path)
+    if err != nil {
+        return CharacterDefinition{}, err
+    }
+
+    value := raw.GetChild("name")
+    return CharacterDefinition{
+        Name: value.GetValue(0),
+    }, nil
+}
+
+func MakePaintownPlayer(name string) (*PaintownCharacter, error) {
+    path := dataPath("players/" + name)
+
+    definitionPath := filepath.Join(path, name + ".txt")
+    definition, err := loadDefinition(definitionPath)
+    if err != nil {
+        return nil, err
+    }
+
+    player := &PaintownCharacter{
+        Definition: definition,
+    }
+
+    return player, nil
 }
 
 func runGame(yield coroutine.YieldFunc, setDraw func(drawer DrawFunc) DrawFunc) error {
