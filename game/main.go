5748edaa66e2ff0973047bc6bf66468025cd6546
Jon Rafkind <jon@rafkind.com>
2025-01-15 16:06:32 -0800

    import ebiten, make window

--- game/main.go
+++ game/main.go
@@ -2,8 +2,35 @@ package main
 
 import (
     "log"
+
+    "github.com/hajimehoshi/ebiten/v2"
 )
+
+type Engine struct {
+}
+
+func (engine *Engine) Update() error {
+    return nil
+}
+
+func (engine *Engine) Draw(screen *ebiten.Image) {
+}
+
+func (engine *Engine) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+    return 1024, 768
+}
 
 func main(){
     log.SetFlags(log.Ldate | log.Lmicroseconds | log.Lshortfile)
+
+    ebiten.SetWindowSize(1024, 768)
+    ebiten.SetWindowTitle("Paintown")
+    ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+
+    engine := &Engine{}
+
+    err := ebiten.RunGame(engine)
+    if err != nil {
+        log.Printf("Error: %v", err)
+    }
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/kazzmir/paintown
 
 go 1.23.0
+
+require github.com/hajimehoshi/ebiten/v2 v2.8.6
+
+require (
+	github.com/ebitengine/gomobile v0.0.0-20241016134836-cc2e38a7c0ee // indirect
+	github.com/ebitengine/hideconsole v1.0.0 // indirect
+	github.com/ebitengine/purego v0.8.2 // indirect
+	github.com/jezek/xgb v1.1.1 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+)
