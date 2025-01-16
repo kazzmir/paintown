20cda0ffaa3df53ae20cf2133364344b683cb8df
Jon Rafkind <jon@rafkind.com>
2025-01-15 16:12:02 -0800

    show background

--- game/main.go
+++ game/main.go
@@ -2,22 +2,71 @@ package main
 
 import (
     "log"
+    "os"
+    "image"
+    "image/png"
 
     "github.com/hajimehoshi/ebiten/v2"
+    "github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
 type Engine struct {
+    Background *ebiten.Image
+}
+
+func dataPath(path string) string {
+    return "paintown-data/" + path
+}
+
+func loadPng(path string) (image.Image, error) {
+    file, err := os.Open(dataPath(path))
+    if err != nil {
+        return nil, err
+    }
+    defer file.Close()
+
+    img, err := png.Decode(file)
+    if err != nil {
+        return nil, err
+    }
+
+    return img, nil
+}
+
+func MakeEngine() (*Engine, error) {
+    backgroundPng, err := loadPng("menu/paintown.png")
+    if err != nil {
+        return nil, err
+    }
+
+    background := ebiten.NewImageFromImage(backgroundPng)
+
+    engine := &Engine{
+        Background: background,
+    }
+
+    return engine, nil
 }
 
 func (engine *Engine) Update() error {
+    keys := inpututil.AppendJustPressedKeys(nil)
+
+    for _, key := range keys {
+        if key == ebiten.KeyEscape || key == ebiten.KeyCapsLock {
+            return ebiten.Termination
+        }
+    }
+
     return nil
 }
 
 func (engine *Engine) Draw(screen *ebiten.Image) {
+    var options ebiten.DrawImageOptions
+    screen.DrawImage(engine.Background, &options)
 }
 
 func (engine *Engine) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-    return 1024, 768
+    return 640, 480
 }
 
 func main(){
@@ -27,9 +76,12 @@ func main(){
     ebiten.SetWindowTitle("Paintown")
     ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
 
-    engine := &Engine{}
+    engine, err := MakeEngine()
+    if err != nil {
+        log.Printf("Error: %v", err)
+    }
 
-    err := ebiten.RunGame(engine)
+    err = ebiten.RunGame(engine)
     if err != nil {
         log.Printf("Error: %v", err)
     }
