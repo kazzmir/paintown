3ed475c4d2e76dc3121e95014e7514d03ad52460
Jon Rafkind <jon@rafkind.com>
2026-03-20 09:23:24 -0700

    put sound back in pool

--- game/paintown/audio.go
+++ game/paintown/audio.go
@@ -5,6 +5,7 @@ import (
     "bufio"
     "os"
     "io"
+    "time"
 
     "github.com/kazzmir/paintown/game/data"
 
@@ -24,7 +25,7 @@ func MakeAudioManager(context *audiolib.Context) *AudioManager {
     }
 }
 
-func (audio *AudioManager) LoadSound(path string) (*audiolib.Player, error) {
+func (audio *AudioManager) LoadSound(path string) (*audiolib.Player, func(), error) {
     pool, exists := audio.sounds[path]
     if !exists {
         fullPath := data.DataPath(path)
@@ -44,7 +45,7 @@ func (audio *AudioManager) LoadSound(path string) (*audiolib.Player, error) {
         })()
 
         if err != nil {
-            return nil, err
+            return nil, func(){}, err
         }
 
         pool = &sync.Pool{
@@ -57,16 +58,28 @@ func (audio *AudioManager) LoadSound(path string) (*audiolib.Player, error) {
 
     player := pool.Get().(*audiolib.Player)
     player.Rewind()
-    return player, nil
+
+    putBack := func() {
+        pool.Put(player)
+    }
+
+    return player, putBack, nil
 }
 
 func (audio *AudioManager) PlaySound(path string) error {
     if path != "" {
-        sound, err := audio.LoadSound(path)
+        sound, putBack, err := audio.LoadSound(path)
         if err != nil {
             return err
         } else {
             sound.Play()
+            go func() {
+                for sound.IsPlaying() {
+                    time.Sleep(100 * time.Millisecond)
+                }
+
+                putBack()
+            }()
         }
     }
 
